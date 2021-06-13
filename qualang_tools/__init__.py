4e4154d3340163be6c72abb8c32af5ad74958d46
gal <gal@quantum-machines.co>
2021-06-13 15:59:57 +0300

    edited the exported symbol list in __init__

---
+++ qualang_tools/__init__.py
@@ -0,0 +1,5 @@
+from qualang_tools.bakery.bakery import baking
+from qualang_tools.bakery.xeb import XEB
+from qualang_tools.bakery.randomized_benchmark import RBOneQubit, c1_ops,find_revert_op,RBSequence
+
+__all__ = ["baking", "XEB", "c1_ops","RBOneQubit","RBSequence","find_revert_op"]
