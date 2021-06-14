59c26cb98345e72c7e41af764437f46d79208497
Lior Ella <lior@quantum-machines.co>
2021-06-14 18:28:11 +0300

    revised XEB

--- qualang_tools/bakery/xeb.py
+++ qualang_tools/bakery/xeb.py
@@ -1,45 +1,102 @@
-from typing import List
+from dataclasses import dataclass
+from typing import List, Callable, Union
 
-from qualang_tools.bakery.bakery import baking
+from entropylab_qpudb import QuaConfig
+
+from qualang_tools.bakery.bakery import baking, Baking
 import numpy as np
 
-rnd_gate_map = {0: "sx", 1: "sy", 2: "sw"}
+rnd_gate_list = ["sx", "sy", "sw"]
+
+op_func = Callable[[Baking], None]
+
+
+@dataclass
+class XEBOpsSingleQubit:
+    """
+    A class for representing the XEB operations. Each callable corresponds to a function with a baking object as argument,
+    and performs the operations in the body of the function.
+
+    Operations defined in this class correspond to single qubit operations
+    """
+    sx: op_func
+    sy: op_func
+    sw: op_func
+
 
 
 class XEB:
-    def __init__(self, config: dict, m_max: int, qe_list: List[str]):
+    def __init__(self,
+                 config: dict,
+                 m_max: int,
+                 q1_ops: XEBOpsSingleQubit,
+                 q2_ops: XEBOpsSingleQubit,
+                 two_qubit_op: op_func,
+                 align_op: op_func
+                 ):
         """
         Class instance for cross-entropy benchmarking sequence generation.
-        :param config Configuration file
-        :param m_max Maximum length of XEB sequence
-        :param qe_list List of quantum elements used to do the sequence ([qubit1, qubit2, coupler])
+
+        :param config: Configuration file
+        :param m_max: Maximum length of XEB sequence
+        :param q1_ops: operations dataclass which maps single qubit operations to series of operations for qubit 1
+        :param q2_ops: operations dataclass which maps single qubit operations to series of operations for qubit 2
+        :param two_qubit_op: a callable with baking object as argument that performs the two qubit operation
+        :param align_op: a callable with baking object as argument that performs an align operation on all quantum elements
 
         """
-        self.config = config
-        self.qe_list = qe_list
+        self.config: QuaConfig = config
         self.m_max = m_max
         self.duration_tracker = [0] * m_max
-        self.operations_list = {qe: [] for qe in qe_list}
-        self.baked_sequence = self.generate_xeb_sequence()
+        self.operations_list = {qe: [] for qe in ["q1", "q2"]}
+        self.baked_sequence = self._generate_xeb_sequence(q1_ops, q2_ops, two_qubit_op, align_op)
 
-    def generate_xeb_sequence(self):
+    def _generate_xeb_sequence(self, q1_ops, q2_ops, two_qubit_op, align_op):
         rand_seq1 = np.random.randint(3, size=self.m_max)
         rand_seq2 = np.random.randint(3, size=self.m_max)
-        q1 = self.qe_list[0]
-        q2 = self.qe_list[1]
-        coupler = self.qe_list[2]
-        with baking(self.config) as b:
+
+        with baking(self.config) as b_2_qubit:
+            two_qubit_op(b_2_qubit)
+
+        b_2_qubit_len = get_total_len(b_2_qubit)
+
+        b_q1 = []
+        q1_len = 0
+        for op in rnd_gate_list:
+            with baking(self.config) as btemp:
+                getattr(q1_ops, op)(btemp)
+            b_q1.append(btemp)
+            q1_len = max(q1_len, get_total_len(btemp))
+
+        b_q2 = []
+        q2_len = 0
+        for op in rnd_gate_list:
+            with baking(self.config) as btemp:
+                getattr(q2_ops, op)(btemp)
+            b_q2.append(btemp)
+            q2_len = max(q2_len, get_total_len(btemp))
+
+        with baking(self.config) as b_main:
             i = 0
             for rnd1, rnd2 in zip(rand_seq1, rand_seq2):
-                b.align(q1, q2, coupler)
-                b.play(rnd_gate_map[rnd1], q1)
-                b.play(rnd_gate_map[rnd2], q2)
-                b.align(q1, q2, coupler)
-                b.play("coupler_op", coupler)
-                self.operations_list[q1].append(rnd_gate_map[rnd1])
-                self.operations_list[q2].append(rnd_gate_map[rnd2])
-                self.operations_list[q2].append("coupler_op")
-
-                self.duration_tracker[i] = b.get_current_length(coupler)
+                align_op(b_main)
+                play_all_ops(b_main, b_q1[rnd1])
+                play_all_ops(b_main, b_q2[rnd2])
+                align_op(b_main)
+                play_all_ops(b_main, b_2_qubit)
+                align_op(b_2_qubit)
+                self.operations_list["q1"].append(rnd_gate_list[rnd1])
+                self.operations_list["q2"].append(rnd_gate_list[rnd2])
+                self.duration_tracker[i] = max(q1_len, q2_len) + b_2_qubit_len
                 i += 1
-        return b
+        self.duration_tracker = np.cumsum(self.duration_tracker).tolist()
+        return b_main
+
+
+def play_all_ops(current_bake, sub_bake):
+    for element in sub_bake.elements:
+        current_bake.play(sub_bake.operations[element], element)
+
+
+def get_total_len(baking_sequence):
+    return max(baking_sequence.get_Op_length(element) for element in baking_sequence.elements)
