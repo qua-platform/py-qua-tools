566bccb5a94f671e16f24ffae856809a79be9bf8
Lior Ella <lior@quantum-machines.co>
2021-06-14 18:18:48 +0300

    added test

--- qualang_tools/tests/test_bakery.py
+++ qualang_tools/tests/test_bakery.py
@@ -6,8 +6,9 @@ from qm import SimulationConfig
 from qualang_tools.bakery.bakery import baking
 
 
-def get_config():
-    config = {
+@pytest.fixture
+def config():
+    return {
         "version": 1,
         "controllers": {
             "con1": {
@@ -20,9 +21,10 @@ def get_config():
         "elements": {
             "qe1": {
                 "singleInput": {"port": ("con1", 1)},
-                "intermediate_frequency": 5e6,
+                "intermediate_frequency": 0,
                 "operations": {
                     "playOp": "constPulse",
+                    "a_pulse": "arb_pulse1"
                 },
             },
         },
@@ -32,31 +34,53 @@ def get_config():
                 "length": 1000,  # in ns
                 "waveforms": {"single": "const_wf"},
             },
+            "arb_pulse1": {
+                "operation": "control",
+                "length": 100,  # in ns
+                "waveforms": {"single": "arb_wf"},
+            },
         },
         "waveforms": {
             "const_wf": {"type": "constant", "sample": 0.2},
+            "arb_wf": {"type": "arbitrary", "samples": [i / 200 for i in range(100)]},
         },
     }
-    return config
 
 
 def simulate_program_and_return(config, prog, duration=20000):
     qmm = QuantumMachinesManager()
     qmm.close_all_quantum_machines()
-    job = qmm.simulate(config, prog, SimulationConfig(duration))
-    samples = job.get_simulated_samples()
-    return samples
+    job = qmm.simulate(config, prog, SimulationConfig(duration, include_analog_waveforms=True))
+    return job
 
 
-def test_simple_bake():
-    config = get_config()
+def test_simple_bake(config):
     with baking(config=config) as b:
         for x in range(10):
-            b.add_Op(f"new_op_{x}", "qe1", samples=[1, 0, 1, 0])
-            b.play(f"new_op_{x}", "qe1")
+            b.add_Op(f'new_op_{x}', "qe1", samples=[1, 0, 1, 0])
+            b.play(f'new_op_{x}', "qe1")
 
     with program() as prog:
         b.run()
 
-    samples = simulate_program_and_return(config, prog)
-    assert len(samples.con1.analog["1"]) == 80000
+    job = simulate_program_and_return(config, prog)
+    samples = job.get_simulated_samples()
+    assert len(samples.con1.analog['1']) == 80000
+
+
+def test_bake_with_macro(config):
+    def play_twice(b):
+        b.play('a_pulse', 'qe1')
+        b.play('a_pulse', 'qe1')
+
+    with baking(config=config) as b:
+        play_twice(b)
+
+    with program() as prog:
+        b.run()
+
+    job = simulate_program_and_return(config, prog, 200)
+    samples = job.get_simulated_samples()
+    tstamp = int(job.simulated_analog_waveforms()['controllers']['con1']['ports']['1'][0]['timestamp'])
+    assert all(
+        samples.con1.analog['1'][tstamp:tstamp + 200] == [i / 200 for i in range(100)] + [i / 200 for i in range(100)])
