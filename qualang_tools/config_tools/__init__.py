80d74fc0345c8755b72cf0166b88ec89265fbdf8
yomach <53370976+yomach@users.noreply.github.com>
2021-10-17 20:20:00 +0300

    Integration weights tools (#25)
    * integration weights tools
    
    * renamed
    
    * add qm-qua to dependencies
    
    * tests is not a package
    
    * added integration weights
    
    * format

---
+++ qualang_tools/config_tools/__init__.py
@@ -0,0 +1,6 @@
+from qualang_tools.config_tools.integration_weights_tools import (
+    convert_integration_weights,
+    compress_integration_weights,
+)
+
+__all__ = ["convert_integration_weights", "compress_integration_weights"]
