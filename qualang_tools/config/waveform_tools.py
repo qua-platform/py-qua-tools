ed04e964cdeb2772cd6ea816dfd5c8cfea9aefd2
Yoav Romach <yomach@gmail.com>
2022-07-03 09:36:58 +0300

    Deal with alpha=0 case

--- qualang_tools/config/waveform_tools.py
+++ qualang_tools/config/waveform_tools.py
@@ -18,13 +18,15 @@ def drag_gaussian_pulse_waveforms(
     :param int length: The pulse length in ns.
     :param float sigma: The gaussian standard deviation.
     :param float alpha: The DRAG coefficient.
-    :param float detuning: The frequency shift to correct for AC stark shift, in Hz.
     :param float delta: f_21 - f_10 - The differences in energy between the 2-1 and the 1-0 energy levels, in Hz.
+    :param float detuning: The frequency shift to correct for AC stark shift, in Hz.
     :param bool subtracted: If true, returns a subtracted Gaussian, such that the first and last points will be at 0
         volts. This reduces high-frequency components due to the initial and final points offset. Default is true.
     :return: Returns a tuple of two lists. The first list is the I waveform (real part) and the second is the
         Q waveform (imaginary part)
     """
+    if alpha != 0 and delta == 0:
+        raise Exception("Cannot create a DRAG pulse with `delta=0`")
     t = np.arange(length, dtype=int)  # An array of size pulse length in ns
     center = (length - 1) / 2
     gauss_wave = amplitude * np.exp(
@@ -37,12 +39,12 @@ def drag_gaussian_pulse_waveforms(
     )  # The derivative of gaussian
     if subtracted:
         gauss_wave = gauss_wave - gauss_wave[-1]  # subtracted gaussian
-    z = gauss_wave + 1j * gauss_der_wave * (
-        alpha / (delta - detuning)
-    )  # The complex DRAG envelope
-    z *= np.exp(
-        1j * 2 * np.pi * detuning * t * 1e-9
-    )  # The complex detuned DRAG envelope
+    z = gauss_wave + 1j * 0
+    if alpha != 0:
+        # The complex DRAG envelope:
+        z += 1j * gauss_der_wave * (alpha / (delta - detuning))
+        # The complex detuned DRAG envelope:
+        z *= np.exp(1j * 2 * np.pi * detuning * t * 1e-9)
     I_wf = z.real.tolist()  # The `I` component is the real part of the waveform
     Q_wf = z.imag.tolist()  # The `Q` component is the imaginary part of the waveform
     return I_wf, Q_wf
@@ -61,11 +63,13 @@ def drag_cosine_pulse_waveforms(amplitude, length, alpha, delta, detuning=0):
     :param float amplitude: The amplitude in volts.
     :param int length: The pulse length in ns.
     :param float alpha: The DRAG coefficient.
-    :param float detuning: The frequency shift to correct for AC stark shift, in Hz.
     :param float delta: f_21 - f_10 - The differences in energy between the 2-1 and the 1-0 energy levels, in Hz.
+    :param float detuning: The frequency shift to correct for AC stark shift, in Hz.
     :return: Returns a tuple of two lists. The first list is the I waveform (real part) and the second is the
         Q waveform (imaginary part)
     """
+    if alpha != 0 and delta == 0:
+        raise Exception("Cannot create a DRAG pulse with `delta=0`")
     t = np.arange(length, dtype=int)  # An array of size pulse length in ns
     end_point = length - 1
     cos_wave = (
@@ -77,12 +81,12 @@ def drag_cosine_pulse_waveforms(amplitude, length, alpha, delta, detuning=0):
         * (2 * np.pi / end_point * 1e9)
         * np.sin(t * 2 * np.pi / end_point)
     )  # The derivative of cosine function
-    z = cos_wave + 1j * sin_wave * (
-        alpha / (delta - detuning)
-    )  # The complex DRAG envelope
-    z *= np.exp(
-        1j * 2 * np.pi * detuning * t * 1e-9
-    )  # The complex detuned DRAG envelope
+    z = cos_wave + 1j * 0
+    if alpha != 0:
+        # The complex DRAG envelope:
+        z += 1j * sin_wave * (alpha / (delta - detuning))
+        # The complex detuned DRAG envelope:
+        z *= np.exp(1j * 2 * np.pi * detuning * t * 1e-9)
     I_wf = z.real.tolist()  # The `I` component is the real part of the waveform
     Q_wf = z.imag.tolist()  # The `Q` component is the imaginary part of the waveform
     return I_wf, Q_wf
