91022d75d270e3068e57aa612b8819617f2f4d96
Michal Goldenshtein <michal@quantum-machines.co>
2022-07-31 18:49:39 +0300

    merging the two files and normalize the vectors

--- qualang_tools/plot/__init__.py
+++ qualang_tools/plot/__init__.py
@@ -1,6 +1,6 @@
 from qualang_tools.plot.plot import interrupt_on_close
 from qualang_tools.plot.Fitting.fitting import *
-from qualang_tools.plot.Fitting.fitting_functions import *
+
 
 __all__ = [
     "interrupt_on_close",
@@ -8,9 +8,5 @@ __all__ = [
     "Plot",
     "Save",
     "Read",
-    "fit_ramsey",
-    "fit_linear",
-    "fit_phase_resonator_spectroscopy",
-    "fit_reflection_resonator_spectroscopy",
-    "fit_transmission_resonator_spectroscopy",
+
 ]
