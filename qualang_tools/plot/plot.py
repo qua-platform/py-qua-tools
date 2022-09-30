d61b198de97fa6510f1f842e4df04b13bba89d2a
TheoQM <theo@quantum-machines.co>
2022-09-30 14:07:20 +0200

    Add 1D and 2D plots for I and Q

--- qualang_tools/plot/plot.py
+++ qualang_tools/plot/plot.py
@@ -1,4 +1,4 @@
-"""Tools to help handling plots from QUA programs.
+"""Tools to help to handle plots from QUA programs.
 
 Content:
     - interrupt_on_close: Allows to interrupt the execution and free the console when closing the live-plotting figure.
@@ -7,9 +7,161 @@ Content:
 
 from typing import List
 import numpy as np
+from scipy import signal
 import plotly.graph_objects as go
 from matplotlib import pyplot as plt
 from qm.QmJob import QmJob
+
+
+def plot_amplitude_and_phase_2d(
+    x,
+    y,
+    I,
+    Q,
+    amp_and_phase: bool = True,
+    x_label: str = None,
+    y_label: str = None,
+    title: str = None,
+    plot_options: dict = None,
+):
+    """
+    Plot 2D maps of either 'I' and 'Q' or the corresponding amplitude and phase in a single figure.
+
+    :param x: 1D numpy array representing the x-axis.
+    :param y: 1D numpy array representing the y-axis.
+    :param I: 2D numpy array representing the 'I' quadrature.
+    :param Q: 2D numpy array representing the 'Q' quadrature.
+    :param amp_and_phase: if True, plots the amplitude [np.sqrt(I**2 + Q**2)] and phase [signal.detrend(np.unwrap(np.angle(I + 1j * Q)))] instead of I and Q.
+    :param x_label: name for the x-axis label.
+    :param y_label: name for the y-axis label.
+    :param title: title of the plot.
+    :param plot_options: dictionary containing various plotting options ['fontsize', 'cmap', 'figsize'].
+    :return: the matplotlib figure object.
+    """
+    _plot_options = {
+        "fontsize": 14,
+        "cmap": "magma",
+        "figsize": None,
+    }
+    if plot_options is not None:
+        for key in plot_options:
+            if key in _plot_options.keys():
+                _plot_options[key] = plot_options[key]
+            else:
+                raise ValueError(
+                    f"The key {key} in 'plot_options' doesn't exists. The available options are {[option for option in _plot_options.keys()]}"
+                )
+    if amp_and_phase:
+        z1 = np.sqrt(I**2 + Q**2)
+        z2 = signal.detrend(np.unwrap(np.angle(I + 1j * Q)))
+        z1_label = "Amplitude [a.u.]"
+        z2_label = "Phase [rad]"
+    else:
+        z1 = I
+        z2 = Q
+        z1_label = "I [a.u.]"
+        z2_label = "Q [a.u.]"
+
+    fig = plt.figure(figsize=_plot_options["figsize"])
+    plt.suptitle(title, fontsize=_plot_options["fontsize"] + 2)
+    ax1 = plt.subplot(211)
+    plt.cla()
+    plt.title(z1_label)
+    plt.pcolor(x, y, z1, cmap=_plot_options["cmap"])
+    plt.ylabel(y_label, fontsize=_plot_options["fontsize"])
+    plt.xticks(fontsize=_plot_options["fontsize"])
+    plt.yticks(fontsize=_plot_options["fontsize"])
+    plt.colorbar()
+    plt.subplot(212, sharex=ax1)
+    plt.cla()
+    plt.title(z2_label)
+    plt.pcolor(x, y, z2, cmap=_plot_options["cmap"])
+    plt.xlabel(x_label, fontsize=_plot_options["fontsize"])
+    plt.ylabel(y_label, fontsize=_plot_options["fontsize"])
+    plt.xticks(fontsize=_plot_options["fontsize"])
+    plt.yticks(fontsize=_plot_options["fontsize"])
+    plt.colorbar()
+    plt.tight_layout()
+    return fig
+
+
+def plot_amplitude_and_phase_1d(
+    x: np.ndarray,
+    I: np.ndarray,
+    Q: np.ndarray,
+    amp_and_phase: bool = True,
+    x_label: str = None,
+    title: str = None,
+    plot_options: dict = None,
+):
+    """
+    Plot 1D graphs of either 'I' and 'Q' or the corresponding amplitude and phase in a single figure.
+
+    :param x: 1D numpy array representing the x-axis.
+    :param I: 1D numpy array representing the 'I' quadrature.
+    :param Q: 1D numpy array representing the 'Q' quadrature.
+    :param amp_and_phase: if True, plots the amplitude [np.sqrt(I**2 + Q**2)] and phase [signal.detrend(np.unwrap(np.angle(I + 1j * Q)))] instead of I and Q.
+    :param x_label: name for the x-axis label.
+    :param title: title of the plot.
+    :param plot_options: dictionary containing various plotting options ['fontsize', 'color', 'marker', 'linewidth', 'figsize'].
+    :return: the matplotlib figure object.
+    """
+    _plot_options = {
+        "fontsize": 14,
+        "color": "b",
+        "marker": "o",
+        "linewidth": 0,
+        "figsize": None,
+    }
+    if plot_options is not None:
+        for key in plot_options:
+            if key in _plot_options.keys():
+                _plot_options[key] = plot_options[key]
+            else:
+                raise ValueError(
+                    f"The key '{key}' in 'plot_options' doesn't exists. The available options are {[option for option in _plot_options.keys()]}"
+                )
+
+    if amp_and_phase:
+        y1 = np.sqrt(I**2 + Q**2)
+        y2 = signal.detrend(np.unwrap(np.angle(I + 1j * Q)))
+        y1_label = "Amplitude [a.u.]"
+        y2_label = "Phase [rad]"
+    else:
+        y1 = I
+        y2 = Q
+        y1_label = "I [a.u.]"
+        y2_label = "Q [a.u.]"
+
+    fig = plt.figure(figsize=_plot_options["figsize"])
+    plt.suptitle(title, fontsize=_plot_options["fontsize"] + 2)
+    ax1 = plt.subplot(211)
+    plt.cla()
+    plt.plot(
+        x,
+        y1,
+        color=_plot_options["color"],
+        marker=_plot_options["marker"],
+        linewidth=_plot_options["linewidth"],
+    )
+    plt.ylabel(y1_label, fontsize=_plot_options["fontsize"])
+    plt.xticks(fontsize=_plot_options["fontsize"])
+    plt.yticks(fontsize=_plot_options["fontsize"])
+    plt.subplot(212, sharex=ax1)
+    plt.cla()
+    plt.plot(
+        x,
+        y2,
+        color=_plot_options["color"],
+        marker=_plot_options["marker"],
+        linewidth=_plot_options["linewidth"],
+    )
+    plt.xlabel(x_label, fontsize=_plot_options["fontsize"])
+    plt.ylabel(y2_label, fontsize=_plot_options["fontsize"])
+    plt.xticks(fontsize=_plot_options["fontsize"])
+    plt.yticks(fontsize=_plot_options["fontsize"])
+    plt.tight_layout()
+    return fig
 
 
 def plot_channel(ax, data: np.ndarray, title: str):
@@ -112,7 +264,6 @@ def plot_ar_attempts(res_handles, qubits: List[int], attempts_data: str, **hist_
     :param attempts_data: base name for the data containing the number of attempts for each qubit (ex: 'AR_attempts_qb').
     :param qubits: list of the qubit numbers involved in the experiment as defined in the config (ex: [0, 2, 4]).
     :param hist_kwargs: arguments to be passed to the 'hist' plot.
-    :return:
     """
     ar_data = {
         q: res_handles.get(attempts_data + str(q)).fetch_all()["value"] for q in qubits
@@ -133,7 +284,6 @@ def interrupt_on_close(figure, current_job):
 
     :param figure: the python figure instance correponding to the live-plotting figure.
     :param current_job: a ``QmJob`` object (see QM Job API) corresponding to the running job.
-    :return: None.
     """
 
     def on_close(event):
