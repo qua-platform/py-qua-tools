d9972d5c02b339cca546f5409b7fa812a8109946
joseph hickie <joseph@quantum-machines.co>
2022-10-03 13:20:01 +0200

    test joe

--- qualang_tools/analysis/discriminator.py
+++ qualang_tools/analysis/discriminator.py
@@ -10,6 +10,9 @@ def _false_detections(threshold, Ig, Ie):
         false_detections_var = np.sum(Ig < threshold) + np.sum(Ie > threshold)
     return false_detections_var
 
+def print_hello():
+    print("hello")
+
 
 def two_state_discriminator(Ig, Qg, Ie, Qe, b_print=True, b_plot=True):
     """
