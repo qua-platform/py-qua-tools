a823fc1ccdfaf8864549a84ded0225311c51c258
Lorenzo Peri <lorenzo.peri17@gmail.com>
2022-10-28 16:29:35 +0100

    Implemented nanosecond

--- qualang_tools/units/units.py
+++ qualang_tools/units/units.py
@@ -7,16 +7,56 @@ Content:
     - raw2volts: converts raw data to volts.
 """
 
+from qm.program._Program import _Program
+from inspect import stack
+from warnings import warn
+
+class _nanosecond:
+
+    def __init__(self, round_up : bool = False):
+        self.round_up = round_up
+
+    def _get_value(self) -> float:
+
+        for frameinfo in stack():
+            
+            for var in frameinfo.frame.f_locals.values():
+
+                if isinstance(var, _Program): # we have a qua program being declared somewhere
+                    if var._is_in_scope: # this is set by __enter__ and unset by __exit__
+                        return 0.25 # now it is in clock cycles
+        else:
+            return 1.0 # this is in nanosecods
+
+    def __mul__(self, other : int | float ) -> int:
+
+        value = self._get_value()
+
+        result, remainder =  divmod(other * value, 1)
+
+        if remainder != 0:
+
+            if self.round_up:
+                result +=1
+
+            warn(f'In a qua program we set clock cycles. Value {other} was found NOT to be divisible by 4ns. The timing will be set to {result/value} instead', RuntimeWarning)
+            
+        return int(result)
+
+    def __rmul__(self, other : int | float ) -> int :
+        return self.__mul__(other)
+
+    def __get__(self, obj, type=None):
+        return self._get_value()
 
 class unit:
+
+
     def __init__(self):
         # Time units
-        self.ns = 1
-        self.us = 1000
-        self.ms = 1000_000
-        self.s = 1000_000_000
-        self.cc = 1 / 4
-
+        self._ns = _nanosecond()
+        self.cc = 1/4
+        
         # Frequency units
         self.mHz = 0.001
         self.Hz = 1
@@ -28,6 +68,26 @@ class unit:
         self.V = 1
         self.mV = 1e-3
         self.uV = 1e-6
+
+    @property
+    def ns(self):
+        return self._ns._get_value()
+
+    @property
+    def us(self):
+        return 1e3 * self._ns
+    
+    @property
+    def ms(self):
+        return 1e6 * self._ns
+
+    @property
+    def s(self):
+        return 1e9 * self._ns
+
+    @property
+    def clock_cycle(self):
+        return 4 * self._ns
 
     # conversion functions
     def to_clock_cycles(self, t):
