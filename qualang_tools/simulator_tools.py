a88e686ecb8aa201c58fce1c0e89bf67bb07fd2d
Yoav Romach <yomach@gmail.com>
2022-12-06 18:05:53 +0200

    simulator tools -> better create connections

--- qualang_tools/simulator_tools.py
+++ qualang_tools/simulator_tools.py
@@ -1,7 +1,8 @@
+import numpy as np
 from qm import ControllerConnection, InterOpxChannel
 
 
-def create_simulator_controller_connections(n_controllers, n_connections=12):
+def create_simulator_controller_connections(n_controllers, n_connections=12, old_method=False, n_actual_controllers=None):
     """
     Creates a list of :class:`~qm.simulate.interface.ControllerConnection` objects with all of the Inter-controllers
     connections according to the `recommended schema
@@ -15,35 +16,66 @@ def create_simulator_controller_connections(n_controllers, n_connections=12):
     :returns: The ControllerConnection object
 
     """
-    if n_controllers == 1:
-        return None
-
-    ncbc = n_connections // (n_controllers - 1)  # ncbc = number_of_connections_between_controllers
-    n_connections = ncbc * (n_controllers - 1)
-    first_controller_list = []
-    first_port_list = []
-    second_controller_list = []
-    second_port_list = []
-    for i in range(n_controllers):
-        first_controller_list = first_controller_list + [i + 1] * (n_connections - i * ncbc)
-
-        first_port_list = first_port_list + [j for j in range(i * ncbc, n_connections)]
-
-        for j in range(i + 1, n_controllers):
-            second_controller_list = second_controller_list + [j + 1] * ncbc
-
-        second_port_list = second_port_list + [j for j in range(i * ncbc, (i + 1) * ncbc)] * (n_controllers - i - 1)
-
-    controller_connections = [
-        ControllerConnection(InterOpxChannel(f"con{i}", j), InterOpxChannel(f"con{k}", l))
-        for i, j, k, l in list(
-            zip(
-                first_controller_list,
-                first_port_list,
-                second_controller_list,
-                second_port_list,
+    if not old_method:
+        if not n_actual_controllers:
+            n_actual_controllers = n_controllers
+        controller_connections = []
+        unused_connection = np.ones((n_controllers, n_connections), dtype=bool)
+        while unused_connection.any():
+            for i in range(n_controllers):
+                for j in range(i + 1, n_controllers):
+                    first_con_port = np.nonzero(unused_connection[i, :])[0]
+                    if first_con_port.size == 0:
+                        break
+                    first_con_port = first_con_port[0]
+                    second_con_port = np.nonzero(unused_connection[j, :])[0]
+                    if second_con_port.size == 0:
+                        break
+                    second_con_port = second_con_port[0]
+                    if i < n_actual_controllers and j < n_actual_controllers:
+                        controller_connections.append(
+                            ControllerConnection(
+                                InterOpxChannel(f"con{i+1}", first_con_port),
+                                InterOpxChannel(f"con{j+1}", second_con_port))
+                        )
+                    print(f"con{i+1}:{first_con_port} <-> con{j+1}, {second_con_port}")
+                    unused_connection[i, first_con_port] = False
+                    unused_connection[j, second_con_port] = False
+    else:
+        ncbc = n_connections // (
+            n_controllers - 1
+        )  # ncbc = number_of_connections_between_controllers
+        n_connections = ncbc * (n_controllers - 1)
+        first_controller_list = []
+        first_port_list = []
+        second_controller_list = []
+        second_port_list = []
+        for i in range(n_controllers):
+            first_controller_list = first_controller_list + [i + 1] * (
+                n_connections - i * ncbc
             )
-        )
-    ]
+
+            first_port_list = first_port_list + [j for j in range(i * ncbc, n_connections)]
+
+            for j in range(i + 1, n_controllers):
+                second_controller_list = second_controller_list + [j + 1] * ncbc
+
+            second_port_list = second_port_list + [
+                j for j in range(i * ncbc, (i + 1) * ncbc)
+            ] * (n_controllers - i - 1)
+
+        controller_connections = [
+            ControllerConnection(
+                InterOpxChannel(f"con{i}", j), InterOpxChannel(f"con{k}", l)
+            )
+            for i, j, k, l in list(
+                zip(
+                    first_controller_list,
+                    first_port_list,
+                    second_controller_list,
+                    second_port_list,
+                )
+            )
+        ]
 
     return controller_connections
