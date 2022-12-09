03e5874898b859b070ced5e179af641472470084
TheoQM <theo@quantum-machines.co>
2022-12-09 12:34:47 +0100

    Add resonator_frequency_vs_flux()

--- qualang_tools/plot/fitting.py
+++ qualang_tools/plot/fitting.py
@@ -1019,6 +1019,182 @@ class Fit:
                 outfile.write(json_object)
         return out
 
+    @staticmethod
+    def resonator_frequency_vs_flux(
+        frequency: Union[np.ndarray, List[float]],
+        flux: Union[np.ndarray, List[float]],
+        data: np.ndarray,
+        guess=None,
+        verbose=False,
+        plot=False,
+        save=False,
+    ):
+        """
+        Create a fit for the resonator frequency versus flux 2D map.
+        Each flux points (line-cuts) is fitted with the reflection_resonator_spectroscopy() function to extract the
+        resonance frequency of the resonator and the evolution of this frequency versu flux if fitted using
+
+        .. math::
+        f(x) = amp * (np.sin((2 * np.pi * f) * frequency + phase))**2 + offset
+
+        for unknown parameters :
+            f - The resonator vs flux oscillating frequency [Hz or GHz / V]
+            phase - The phase [rad]
+            amp - The oscillation amplitude [Hz or GHz]
+            offset -  The resonator frequency offset [Hz or GHz]
+
+        :param frequency: The readout frequency [Hz or GHz]
+        :param flux: The flux biases [V]
+        :param data: Data containing the frequency vs flux map. Must be a 2D array with data.shape=(len(flux), len(frequency)).
+        :param dict guess: Dictionary containing the initial guess for the fitting parameters (guess=dict(T2=20))
+        :param verbose: if True prints the initial guess and fitting results
+        :param plot: if True plots the data and the fitting function
+        :param save: if not False saves the data into a json file
+                     The id of the file is save='id'. The name of the json file is `data_fit_id.json`
+          :return: A dictionary of (fit_func, f, phase, amp, offset)
+
+        """
+        freq = [np.nan for _ in range(len(flux))]
+        # Extract 1D curve f_res vs flux
+        map2 = np.zeros((len(flux), len(frequency)))
+        for i in range(len(flux)):
+            try:
+                f = Fit.reflection_resonator_spectroscopy(frequency, data[i])
+                freq[i] = f["f"][0]
+            except (Exception,):
+                if i > 0:
+                    freq[i] = freq[i - 1]
+                else:
+                    freq[i] = np.mean(frequency)
+            # Get fitted curve on 2D map
+            map2[i][np.argmin(np.abs(frequency - freq[i]))] = -np.max(data)
+        # Normalizing the vectors
+        xn = preprocessing.normalize([flux], return_norm=True)
+        yn = preprocessing.normalize([freq], return_norm=True)
+        x = xn[0][0]
+        y = yn[0][0]
+        x_normal = xn[1][0]
+        y_normal = yn[1][0]
+
+        # Compute the FFT for guessing the frequency
+        fft = np.fft.fft(y)
+        f = np.fft.fftfreq(len(x))
+        # Take the positive part only
+        fft = fft[1 : len(f) // 2]
+        f = f[1 : len(f) // 2]
+        # Finding a guess for the frequency
+        out_freq = f[np.argmax(np.abs(fft))]
+        guess_freq = out_freq / (x[1] - x[0])
+        if np.argmax(np.abs(fft)) < 1:
+            guess_freq = 1 / (x[-1] - x[0])
+
+        # Finding a guess for the offset
+        offset = np.mean(y)
+
+        # Finding a guess for the phase
+        guess_phase = (
+            np.angle(fft[np.argmax(np.abs(fft))]) - guess_freq * 2 * np.pi * x[0]
+        )
+
+        guess_amp = np.max(y) - np.min(y)
+        # Check user guess
+        if guess is not None:
+            for key in guess.keys():
+                if key == "f":
+                    guess_freq = float(guess[key]) * x_normal
+                elif key == "phase":
+                    guess_phase = float(guess[key])
+                elif key == "amp":
+                    guess_amp = float(guess[key]) / y_normal
+                elif key == "offset":
+                    offset = float(guess[key]) / y_normal
+                else:
+                    raise Exception(
+                        f"The key '{key}' specified in 'guess' does not match a fitting parameters for this function."
+                    )
+
+        # Print the initial guess if verbose=True
+        if verbose:
+            print(
+                f"Initial guess:\n"
+                f" f = {guess_freq / x_normal:.3f}, \n"
+                f" phase = {guess_phase:.3f}, \n"
+                f" amplitude = {guess_amp * y_normal:.3f}, \n"
+                f" offset = {offset * y_normal:.3f}"
+            )
+
+        # Fitting function
+        def func(x_var, a0, a1, a2, a3):
+            return (guess_amp * a0) * np.sin(
+                (2 * np.pi * guess_freq * a1) * x_var + a2
+            ) + offset * a3
+
+        def fit_type(x_var, a):
+            return func(x_var, a[0], a[1], a[2], a[3])
+
+        popt, pcov = optimize.curve_fit(
+            func,
+            x,
+            y,
+            p0=[1, 1, guess_phase, 1],
+        )
+        perr = np.sqrt(np.diag(pcov))
+        # Check freq unit
+        if np.max(np.abs(frequency)) < 18:
+            freq_unit = "GHz"
+        elif np.max(np.abs(frequency)) < 18000:
+            freq_unit = "MHz"
+        else:
+            freq_unit = "Hz"
+        # Output the fitting function and its parameters
+        out = {
+            "fit_func": lambda x_var: fit_type(x_var / x_normal, popt) * y_normal,
+            "f": [popt[1] * guess_freq / x_normal, perr[1] * guess_freq / x_normal],
+            "phase": [popt[2] % (2 * np.pi), perr[2] % (2 * np.pi)],
+            "amp": [popt[0] * guess_amp * y_normal, perr[0] * guess_amp * y_normal],
+            "offset": [
+                offset * popt[3] * y_normal,
+                perr[3] * offset * y_normal,
+            ],
+        }
+        # Print the fitting results if verbose=True
+        if verbose:
+            print(
+                f"Fitting results:\n"
+                f" f = {out['f'][0]:.3f} +/- {out['f'][1]:.3f} 1/V, \n"
+                f" phase = {out['phase'][0]:.3f} +/- {out['phase'][1]:.3f} rad, \n"
+                f" amplitude = {out['amp'][0]:.2f} +/- {out['amp'][1]:.3f} {freq_unit}, \n"
+                f" offset = {out['offset'][0]:.2f} +/- {out['offset'][1]:.3f} {freq_unit}"
+            )
+        # Plot the data and the fitting function if plot=True
+        if plot:
+            plt.figure()
+            plt.subplot(211)
+            plt.pcolor(frequency, flux, map2 + data)
+            plt.ylabel("Flux bias [V]")
+            plt.xlabel(f"Readout frequency [{freq_unit}]")
+            plt.title("Readout resonator spec vs flux")
+            plt.subplot(212)
+            plt.plot(flux, freq, "b.")
+            plt.plot(flux, fit_type(x, popt) * y_normal)
+            plt.tight_layout()
+            if np.max(np.abs(flux)) < 1:
+                plt.xlabel("Flux bias [V]")
+            else:
+                plt.xlabel("Flux bias [mV]")
+            plt.ylabel(f"Resonator frequency [{freq_unit}]")
+            plt.tight_layout()
+        # Save the data in a json file named 'data_fit_id.json' if save=id
+        if save:
+            fit_params = dict(itertools.islice(out.items(), 1, len(out)))
+            fit_params["x_data"] = flux.tolist()
+            fit_params["y_data"] = freq
+            fit_params["y_fit"] = (fit_type(x, popt) * y_normal).tolist()
+            json_object = json.dumps(fit_params)
+            with open(f"data_fit_{save}.json", "w") as outfile:
+                outfile.write(json_object)
+        return out
+
 
 class Read:
     """
