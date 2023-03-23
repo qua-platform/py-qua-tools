e62351f07c6c28ffe832c58e5cf7d6e58cbe3e46
Yoav Romach <yomach@gmail.com>
2023-03-23 11:50:54 +0200

    fixed missing capabilities

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import warnings
+from contextlib import contextmanager
+
+import pytest
+
+from qm.api.models.capabilities import ServerCapabilities
+from qm.api.models.info import QuaMachineInfo, ImplementationInfo
+from qm.containers.capabilities_container import create_capabilities_container
+
+
+@contextmanager
+def ignore_deprecation_warnings():
+    warnings.simplefilter("ignore")
+    yield
+    warnings.simplefilter("default")
+
+
+@pytest.fixture(autouse=True)
+def capability_container():
+    container = create_capabilities_container(QuaMachineInfo([], ImplementationInfo("", "", "")))
+    container.capabilities.override(ServerCapabilities(True, True, True, True, True, True, True, True, True, True, True,
+                                                       True, True, True))
+    return container
