adc8a118951062dbf6e969ddce79e03abb5cae3d
TheoQM <theo@quantum-machines.co>
2023-06-23 19:27:36 +0200

    update hello_qcodes witth scale factor

--- examples/Qcodes_drivers/basic-driver/hello_qcodes.py
+++ examples/Qcodes_drivers/basic-driver/hello_qcodes.py
@@ -55,7 +55,7 @@ VP1 = MyCounter("counter1", "Vp1")
 VP2 = MyCounter("counter2", "Vp2")
 
 #####################################
-run = "raw_adc"
+run = "1d"
 # Pass the readout length (in ns) to the class to convert the demodulated/integrated data into Volts
 # and create the setpoint Parameter for raw adc trace acquisition
 opx_instrument.readout_pulse_length(readout_len)
@@ -221,7 +221,7 @@ if run == "1d":
     do0d(
         opx_instrument.run_exp,
         opx_instrument.resume,
-        opx_instrument.get_measurement_parameter(),
+        opx_instrument.get_measurement_parameter(scale_factor=[("I", 1235, "pA"), ("Q", 1235, "pA")]),
         opx_instrument.halt,
         do_plot=True,
         exp=experiment,
