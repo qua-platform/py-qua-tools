8bd85f46117122fda907217ffee7e8d65772f826
TheoQM <theo@quantum-machines.co>
2023-11-13 17:21:24 +0100

    Callable from QUA tests

--- qualang_tools/addons/callable_from_qua/test_feedback.py
+++ qualang_tools/addons/callable_from_qua/test_feedback.py
@@ -3,6 +3,7 @@ import time
 from qm.qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm import SimulationConfig
+from qm import generate_qua_script
 from configuration import *
 import matplotlib.pyplot as plt
 import warnings
@@ -43,17 +44,18 @@ def my_prog(callables_from_qua, qm):
     return prog
 
 batches = CallableFromQUA()
-qmm = QuantumMachinesManager(host="192.168.0.129", cluster_name="Cluster_Bordeaux")
+qmm = QuantumMachinesManager(host="172.16.33.101", cluster_name="Cluster_83")
 qm = qmm.open_qm(config)
 
 def live_plot(res_handles):
     data = res_handles.get("correction").fetch_all()["value"]
-    print(data)
+    # print(data)
     plt.cla()
-    plt.plot(data)
+    plt.plot(data,'ko')
     plt.pause(0.01)
 
 prog = my_prog(batches, qm)
 job = batches.execute(qm, prog, live_plot)
 
+print(generate_qua_script(prog, config)[:1000])
 
