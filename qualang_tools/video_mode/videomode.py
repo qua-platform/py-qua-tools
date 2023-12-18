0fdf0ea67cc66485053a84562f1015c874b22adf
Arthur Strauss <arthurostrauss@gmail.com>
2023-12-18 16:59:16 +0800

    Added docstrings

--- qualang_tools/video_mode/videomode.py
+++ qualang_tools/video_mode/videomode.py
@@ -136,17 +136,15 @@ class ParameterTable:
         for parameter_name, parameter in self.table.items():
             text += f"{parameter_name}: {parameter['value']}, "
         print(text)
+        return {parameter_name: parameter["value"] for parameter_name, parameter in self.table.items()}
 
     def __getitem__(self, item):
         return self.table[item]["var"]
 
     @property
     def variables(self):
-        return [
-            var["var"]
-            for var in self.table.values()
-            if var["declare_expression"] is not None
-        ]
+        """List of the QUA variables corresponding to the parameters in the parameter table."""
+        return [self[item] for item in self.table.keys()]
 
 
 class VideoMode:
@@ -170,9 +168,10 @@ class VideoMode:
 
         Args:
             qm: Quantum Machine object.
-            parameters: Parameter table containing the parameters to be updated and their initial values.
-            job: QM job already executed. If None, the video mode will start the execution of the QUA program through the
-            ```execute``` method of this class.
+            parameters: Dictionary or ParameterTable containing the parameters to be updated and their initial values.
+
+                        Dictionary should be of the form {"parameter_name": initial_parameter_value }. The type of the
+                        QUA variable to be adjusted is automatically inferred from the type of the initial_parameter_value.
         """
         self.qm = qm
         self.job = None
@@ -376,12 +375,21 @@ class VideoMode:
 
     @property
     def variables(self):
+        """List of the QUA variables corresponding to the parameters in the parameter table."""
         return self.parameter_table.variables
 
     def load_parameters(self, pause_program=False):
+        """
+        QUA Macro to be called within QUA program to retrieve updated values for the parameters through IO 1 and IO2.
+        Args:
+        pause_program: Boolean indicating whether the program should be paused while waiting for user input.
+        """
         self.parameter_table.load_parameters(pause_program)
 
     def declare_variables(self):
+        """
+        QUA Macro to create the QUA variables associated with the parameter table.
+        """
         return self.parameter_table.declare_variables()
 
     def __getitem__(self, item):
