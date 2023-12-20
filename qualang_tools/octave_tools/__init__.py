f6e52c361b9208ed937ffd7a9918f1866b2c37cf
TheoQM <theo@quantum-machines.co>
2023-12-20 14:29:25 +0100

    tests  + black and verbose_level

--- qualang_tools/octave_tools/__init__.py
+++ qualang_tools/octave_tools/__init__.py
@@ -1,3 +1,6 @@
-from qualang_tools.octave_tools.octave_tools import get_calibration_parameters, set_correction_parameters
+from qualang_tools.octave_tools.octave_tools import (
+    get_calibration_parameters,
+    set_correction_parameters,
+)
 
 __all__ = ["get_calibration_parameters", "set_correction_parameters"]
