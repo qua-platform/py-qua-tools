b41aefe0648aaa51ee35efadb824fe1171a6ee29
TheoQM <theo@quantum-machines.co>
2023-12-20 19:28:21 +0100

    start readme

--- qualang_tools/octave_tools/octave_tools.py
+++ qualang_tools/octave_tools/octave_tools.py
@@ -6,7 +6,7 @@ from qm.QuantumMachine import QuantumMachine
 
 
 def get_calibration_parameters(
-    path: str,
+    path_to_database: str,
     config: dict,
     element: str,
     LO: float,
@@ -14,7 +14,7 @@ def get_calibration_parameters(
     gain: float,
     verbose_level: int = 2,
 ) -> dict:
-    """Returns the most up-to-date correction parameters in the database, located at the specified path, for the
+    """Returns the most up-to-date correction parameters in the database, located at the specified path_to_database, for the
     specified values of the Octave LO frequency, intermediate frequency and Octave gain.
 
     The correction parameters are returned in a dictionary of the form:
@@ -25,7 +25,7 @@ def get_calibration_parameters(
       * verbose_level=1 will print a warning which will not block the execution  and the returned dictionary will be empty.
       * verbose_level=0 will not print anything and the returned dictionary will be empty.
 
-    :param path: direct path to the calibration database (without '/calibration_db.json').
+    :param path_to_database: direct path to the calibration database (without '/calibration_db.json').
     :param config: the OPX config dictionary.
     :param element: the element to get the correction parameters for.
     :param LO: the LO frequency in Hz.
@@ -36,7 +36,7 @@ def get_calibration_parameters(
     """
     oct_name = config["elements"][element]["RF_inputs"]["port"][0]
     channel = config["elements"][element]["RF_inputs"]["port"][1]
-    cal_db = CalibrationDB(path)
+    cal_db = CalibrationDB(path_to_database)
     lo_cal = cal_db.get_lo_cal((oct_name, channel), LO, gain)
     if_cal = cal_db.get_if_cal((oct_name, channel), LO, gain, IF)
 
@@ -56,7 +56,6 @@ def get_calibration_parameters(
                 print(
                     f"No correction parameters were found for the provided set of LO frequency, IF and gain.\nIn particular, the IF is missing for the specified LO frequency and gain: LO={LO} Hz, IF={IF} Hz gain={gain}."
                 )
-
     else:
         if verbose_level == 2:
             raise Warning(
@@ -71,7 +70,7 @@ def get_calibration_parameters(
 
 
 def set_correction_parameters(
-    path: str,
+    path_to_database: str,
     config: dict,
     element: str,
     LO: float,
@@ -81,14 +80,14 @@ def set_correction_parameters(
     job: Union[QmJob, RunningQmJob] = None,
     verbose_level: int = 2,
 ) -> dict:
-    """Look for the correction parameters in the database, located at the specified path, for the specified values of
+    """Look for the correction parameters in the database, located at the specified path_to_database, for the specified values of
     the Octave LO frequency, intermediate frequency and Octave gain and update the running job.
 
     If the correction parameters are not found in the database:
       * verbose_level=2 will raise a warning which will block the execution.
       * verbose_level=1 will print a warning which will not block the execution and correction settings will be unchanged.
       * verbose_level=0 will not print anything and correction settings will be unchanged.
-    :param path: direct path to the calibration database (without '/calibration_db.json').
+    :param path_to_database: direct path to the calibration database (without '/calibration_db.json').
     :param config: the OPX config dictionary.
     :param element: the element to get the correction parameters for.
     :param LO: the LO frequency in Hz.
@@ -100,7 +99,7 @@ def set_correction_parameters(
     :return: dictionary containing the 'I' and 'Q' offsets and the correction_matrix.
     """
     param = get_calibration_parameters(
-        path, config, element, LO, IF, gain, verbose_level
+        path_to_database, config, element, LO, IF, gain, verbose_level
     )
     if "I" in param["offsets"] and "Q" in param["offsets"]:
         qm.set_output_dc_offset_by_element(
