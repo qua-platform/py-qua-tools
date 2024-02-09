adb49f9f8da25396f4dd3843d08ce494a9c285c8
TheoQM <theo@quantum-machines.co>
2024-02-09 10:39:38 +0100

    Conf_test, basic test

---
+++ tests_against_server/callable_from_qua/test_basic_qua_callable.py
@@ -0,0 +1,23 @@
+from qm.qua import *
+from qualang_tools.callable_from_qua import *
+
+patch_qua_program_addons()
+
+
+registered_calls = []
+@callable_from_qua
+def register_calls(*args, **kwargs):
+    registered_calls.append({"args": args, "kwargs": kwargs})
+
+def test_qua_callable_no_args(qmm, config):
+    registered_calls.clear()
+    with program() as prog:
+        register_calls()
+
+    assert not registered_calls
+    qm = qmm.open_qm(config)
+    job = qm.execute(prog)
+
+    from time import sleep
+    sleep(10)
+    assert registered_calls == [{"args": (), "kwargs": {}}]
