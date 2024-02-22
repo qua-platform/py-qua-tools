9b90b0e0b5713c648b3665e227f28c494eda0da4
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-02-22 09:52:33 +0100

    add xarray data handler

--- qualang_tools/results/data_handler/data_processors.py
+++ qualang_tools/results/data_handler/data_processors.py
@@ -117,3 +117,68 @@ class NumpyArraySaver(DataProcessor):
         else:
             for path, arr in self.data_arrays.items():
                 np.save(data_folder / path.with_suffix(".npy"), arr)
+
+
+class XarraySaver(DataProcessor):
+    merge_arrays: bool = False
+    merged_array_name: str = "xarrays"
+    file_format: str = "hdf5"
+
+    def __init__(self, merge_arrays=None, merged_array_name=None, file_format=None):
+        if merge_arrays is not None:
+            self.merge_arrays = merge_arrays
+        if merged_array_name is not None:
+            self.merged_array_name = merged_array_name
+        if file_format is not None:
+            self.file_format = file_format
+
+        self.data_arrays = {}
+
+    @property
+    def file_suffix(self) -> str:
+        suffixes = {"nc": ".nc", "netcdf": ".nc", "h5": ".h5", "hdf5": ".h5", "zarr": ".zarr"}
+        return suffixes[self.file_format.lower()]
+
+    def process(self, data):
+        import xarray as xr
+
+        self.data_arrays = {}
+
+        for keys, val in iterate_nested_dict(data):
+            if not isinstance(val, xr.Dataset):
+                continue
+
+            path = Path("/".join(keys))
+            self.data_arrays[path] = val
+            if self.merge_arrays:
+                merged_array_name = Path(self.merged_array_name).with_suffix(self.file_suffix)
+                update_nested_dict(data, keys, f"./{merged_array_name}#{path}")
+            else:
+                update_nested_dict(data, keys, f"./{path.with_suffix(self.file_suffix)}")
+        return data
+
+    def save_merged_netcdf_arrays(self, path: Path, arrays: dict):
+        for array_path, array in self.data_arrays.items():
+            try:
+                array.to_netcdf(path, mode="a", group=str(array_path))
+            except ValueError as e:
+                raise ValueError(
+                    f"Error saving merged array {path}. You may neet to first run `pip install netcdf4`"
+                ) from e
+
+    def post_process(self, data_folder: Path):
+        if self.file_suffix not in [".nc", ".h5"]:
+            raise NotImplementedError(f"File format {self.file_format} is not supported")
+
+        if self.merge_arrays:
+            for path, array in self.data_arrays.items():
+                merged_path = data_folder / Path(self.merged_array_name).with_suffix(self.file_suffix)
+                try:
+                    array.to_netcdf(merged_path, mode="a", group=str(path))
+                except ValueError as e:
+                    raise ValueError(
+                        f"Error saving merged array {merged_path}. You may neet to first run `pip install netcdf4`"
+                    ) from e
+        else:
+            for path, array in self.data_arrays.items():
+                array.to_netcdf(data_folder / path.with_suffix(self.file_suffix))
---
+++ tests/data_handler/test_xarray_saver.py
@@ -0,0 +1,92 @@
+from qualang_tools.results.data_handler.data_processors import XarraySaver
+
+
+def test_xarray_saver_no_xarrays():
+    xarray_saver = XarraySaver()
+    data = {"a": 1, "b": 2, "c": 3}
+    assert xarray_saver.process(data) == data
+
+
+def test_xarray_data_saver_suffixes():
+    xarray_saver = XarraySaver()
+    assert xarray_saver.file_format == "hdf5"
+    assert xarray_saver.file_suffix == ".h5"
+
+    xarray_saver = XarraySaver(file_format="hdf5")
+    assert xarray_saver.file_suffix == ".h5"
+
+    xarray_saver = XarraySaver(file_format="nc")
+    assert xarray_saver.file_suffix == ".nc"
+
+    xarray_saver = XarraySaver(file_format="netcdf")
+    assert xarray_saver.file_suffix == ".nc"
+
+    xarray_saver = XarraySaver(file_format="zarr")
+    assert xarray_saver.file_suffix == ".zarr"
+
+
+def test_xarray_saver_merge_netcdf(tmp_path):
+    try:
+        # Test won't work if netCDF4 is not installed
+        import netCDF4
+    except ImportError:
+        return
+
+    import xarray as xr
+
+    data = {"a": 1, "b": 2, "c": xr.Dataset(), "d": xr.Dataset()}
+
+    xarray_saver = XarraySaver(merge_arrays=True, file_format="nc")
+    processed_data = xarray_saver.process(data.copy())
+
+    assert processed_data == {"a": 1, "b": 2, "c": "./xarrays.nc#c", "d": "./xarrays.nc#d"}
+
+    xarray_saver.post_process(data_folder=tmp_path)
+
+    assert (tmp_path / "xarrays.nc").exists()
+
+    xr.load_dataset(tmp_path / "xarrays.nc", group="c")
+    xr.load_dataset(tmp_path / "xarrays.nc", group="d")
+
+
+def test_xarray_saver_merge_hdf5(tmp_path):
+    try:
+        # Test won't work if netCDF4 is not installed
+        import netCDF4
+    except ImportError:
+        return
+
+    import xarray as xr
+
+    data = {"a": 1, "b": 2, "c": xr.Dataset(), "d": xr.Dataset()}
+
+    xarray_saver = XarraySaver(merge_arrays=True, file_format="h5")
+    processed_data = xarray_saver.process(data.copy())
+
+    assert processed_data == {"a": 1, "b": 2, "c": "./xarrays.h5#c", "d": "./xarrays.h5#d"}
+
+    xarray_saver.post_process(data_folder=tmp_path)
+
+    assert (tmp_path / "xarrays.h5").exists()
+
+    xr.load_dataset(tmp_path / "xarrays.h5", group="c")
+    xr.load_dataset(tmp_path / "xarrays.h5", group="d")
+
+
+def test_xarray_saver_no_merge_netcdf(tmp_path):
+    import xarray as xr
+
+    data = {"a": 1, "b": 2, "c": xr.Dataset(), "d": xr.Dataset()}
+
+    xarray_saver = XarraySaver(merge_arrays=False)
+    processed_data = xarray_saver.process(data.copy())
+
+    assert processed_data == {"a": 1, "b": 2, "c": "./c.h5", "d": "./d.h5"}
+
+    xarray_saver.post_process(data_folder=tmp_path)
+
+    assert (tmp_path / "c.h5").exists()
+    assert (tmp_path / "d.h5").exists()
+
+    xr.load_dataset(tmp_path / "c.h5")
+    xr.load_dataset(tmp_path / "d.h5")
