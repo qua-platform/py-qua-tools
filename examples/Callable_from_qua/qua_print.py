5f94875e6efff9d4229063bfe3081b0c91f0db8e
TheoQM <theo@quantum-machines.co>
2024-03-18 14:27:58 +0100

    Example files

--- examples/Callable_from_qua/qua_print.py
+++ examples/Callable_from_qua/qua_print.py
@@ -1,11 +1,10 @@
 from qm.qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
-
 from configuration import *
 from qualang_tools.callable_from_qua import *
 
+# Patch to add the callable from qua functions to the main SDK
 patch_qua_program_addons()
-
 
 # Define your callable_from_qua functions
 @callable_from_qua
@@ -14,22 +13,6 @@ def qua_print(*args):
     for i in range(0, len(args) - 1, 2):
         text += f"{args[i]} = {args[i+1]} | "
     print(text)
-
-#####################################
-#  Open Communication with the QOP  #
-#####################################
-# Open the quantum machine manager
-qmm = QuantumMachinesManager(host="172.16.33.101", cluster_name="Cluster_83")
-# from qm.simulate.credentials import create_credentials
-# from qm.QuantumMachinesManager import QuantumMachinesManager
-#
-# qmm = QuantumMachinesManager(
-#     host="serwan-dd85ae55.dev.quantum-machines.co",
-#     port=443,
-#     credentials=create_credentials(),
-# )
-# Open a quantum machine
-qm = qmm.open_qm(config)
 
 
 ###################
@@ -40,20 +23,30 @@ with program() as prog:
     n1 = declare(int)
     n2 = declare(int)
     I = declare(fixed)
+    I10 = declare(fixed)
     I_st = declare_stream()
     with for_(n1, 0, n1 < 2, n1 + 1):
         with for_(n2, 0, n2 < 3, n2 + 1):
             measure(
                 "readout",
-                "resonator",
+                "qe1",
                 None,
                 integration.full("cos", I, "out1"),
             )
             save(I, I_st)
-            qua_print("n1", n1, "n2", n2, "I", I)
+            assign(I10, Cast.mul_fixed_by_int(I, 10))
+            # Print the value of some variables in the Python terminal
+            qua_print("n1", n1, "n2", n2, "I", I, "I*10", I10)
 
     with stream_processing():
         I_st.save_all("I")
 
-# Execute the QUA program using the local_run context manager
+#####################################
+#  Open Communication with the QOP  #
+#####################################
+# Open the quantum machine manager
+qmm = QuantumMachinesManager(host=qop_ip, cluster_name=cluster_name)
+# Open a quantum machine
+qm = qmm.open_qm(config)
+# Execute the QUA program
 job = qm.execute(prog)
