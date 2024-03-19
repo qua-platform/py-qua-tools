b027e479f9b7b6b5e887853092ebd04bb6c5e485
TheoQM <theo@quantum-machines.co>
2024-03-19 10:14:18 +0100

    fix linting

--- qualang_tools/external_frameworks/qcodes/opx_driver.py
+++ qualang_tools/external_frameworks/qcodes/opx_driver.py
@@ -8,7 +8,6 @@ from qcodes import (
 )
 from qcodes.utils.validators import Numbers, Arrays
 from qm import SimulationConfig, generate_qua_script
-from qm.qua import program
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from qualang_tools.results import wait_until_job_is_paused
 from qualang_tools.results import fetching_tool
