29f08666cd32ec4507ed93c2645c989c82977de0
TheoQM <theo@quantum-machines.co>
2024-04-10 14:33:50 +0200

    update tests with @pytest.mark.parametrize

--- tests_against_server/test_loops.py
+++ tests_against_server/test_loops.py
@@ -3,6 +3,7 @@ from qualang_tools.loops import *
 from qm.qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
 from copy import deepcopy
+import numpy as np
 
 
 @pytest.fixture
@@ -91,19 +92,32 @@ def config():
 
 
 def simulate_program_and_return(config, prog):
-    qmm = QuantumMachinesManager()
+    qmm = QuantumMachinesManager("172.16.33.101", cluster_name = "Cluster_83")
     qm = qmm.open_qm(config)
     job = qm.execute(prog)
     return job
 
 
-def test_qua_arange(config):
-    def prog_maker(arange_param):
-        if float(arange_param[2]).is_integer():
+@pytest.mark.parametrize(["start", "stop" ,"step"],
+                         [ [-0.05, -1, -0.15],
+                           [10, 20, 1],
+                           [20, 71, 2],
+                           [20, 71, 1],
+                           [11, 0, -1],
+                           [0.1, 1, 0.2],
+                           [0, 1, 0.2],
+                           [0, 2, 0.0001],
+                           [-1, 2, 0.00011],
+                           [-1, 1, 0.2],
+                           [0.00015, 1, 0.0001],
+                           [0, -2, -0.001],])
+def test_qua_arange(config, start, stop, step):
+    def prog_maker(_start, _stop, _step):
+        if float(step).is_integer():
             with program() as prog:
                 a = declare(int)
                 a_st = declare_stream()
-                with for_(*qua_arange(a, arange_param[0], arange_param[1], arange_param[2])):
+                with for_(*qua_arange(a, _start, _stop, _step)):
                     update_frequency("resonator", a)
                     play("readout", "resonator")
                     save(a, a_st)
@@ -114,7 +128,7 @@ def test_qua_arange(config):
             with program() as prog:
                 a = declare(fixed)
                 a_st = declare_stream()
-                with for_(*qua_arange(a, arange_param[0], arange_param[1], arange_param[2])):
+                with for_(*qua_arange(a, _start, _stop, _step)):
                     play("readout" * amp(a), "resonator")
                     save(a, a_st)
                 with stream_processing():
@@ -122,41 +136,48 @@ def test_qua_arange(config):
             return prog
 
     cfg = deepcopy(config)
-
-    arange_param_list = [
-        [-0.05, -1, -0.15],
-        [10, 20, 1],
-        [20, 71, 2],
-        [20, 71, 1],
-        [11, 0, -1],
-        [-11, -100, -2],
-        [0.1, 1, 0.2],
-        [0, 1, 0.2],
-        [0, 2, 0.0001],
-        [-1, 2, 0.00011],
-        [-1, 1, 0.2],
-        [0.00015, 1, 0.0001],
-        [0, -2, -0.001],
-        # [-1, 2, 0.00006],
-    ]
-
-    for param in arange_param_list:
-        print(f"Test qua_arange with {param}:")
-        job = simulate_program_and_return(cfg, prog_maker(param))
-        job.result_handles.wait_for_all_values()
-        a_qua = job.result_handles.get("a").fetch_all()["value"]
-        a_list = np.arange(param[0], param[1], param[2])
-        assert len(a_list) == len(a_qua)
-        assert np.allclose(a_list, a_qua, atol=1e-4)
+    job = simulate_program_and_return(cfg, prog_maker(start, stop, step))
+    job.result_handles.wait_for_all_values()
+    a_qua = job.result_handles.get("a").fetch_all()["value"]
+    a_list = np.arange(start, stop, step)
+    assert len(a_list) == len(a_qua)
+    assert np.allclose(a_list, a_qua, atol=1e-4)
 
 
-def test_from_array(config):
-    def prog_maker(list_param):
-        if list_param[1] == "int":
+@pytest.mark.parametrize(["vector", "qua_type"],
+                         [
+                             [np.logspace(np.log10(10000), np.log10(4), 100), "int"],
+                             [np.logspace(np.log10(4), np.log10(10000), 29), "int"],
+                             [np.logspace(np.log10(50), np.log10(12500), 19), "int"],
+                             [np.logspace(np.log10(50000), np.log10(33), 72), "int"],
+                             [np.logspace(6, 4, 19), "int"],
+                             [np.logspace(3, 6, 199), "int"],
+                             [np.logspace(-3, 0, 99), "fixed"],
+                             [np.logspace(-3.5, -1, 11), "fixed"],
+                             [np.logspace(0.5, -0.5, 22), "fixed"],
+                             [np.logspace(0.5, -3.5, 21), "fixed"],
+                             [np.arange(-7.0547, -2.2141, 0.1015), "fixed"],
+                             [np.arange(-0.05, -1, -0.15), "fixed"],
+                             [np.arange(-1, 2, 0.0006), "fixed"],
+                             [np.arange(-11, -100, -2), "int"],
+                             [np.arange(20, 71, 2), "int"],
+                             [np.linspace(20, 71, 52), "int"],
+                             [np.linspace(0.1, 1, 6), "fixed"],
+                             [np.arange(10, 20, 1), "int"],
+                             [np.arange(20, 71, 1), "int"],
+                             [np.arange(0, 71, 2), "int"],
+                             [np.arange(0, 1, 0.1), "fixed"],
+                             [np.arange(0, 1, 0.2), "fixed"],
+                             [np.arange(0.1, 1, 0.2), "fixed"],
+                             # [np.arange(0.00015, 1, 0.0001), "fixed"],
+                         ])
+def test_from_array(config, vector, qua_type):
+    def prog_maker(_vector, _qua_type):
+        if _qua_type == "int":
             with program() as prog:
                 a = declare(int)
                 a_st = declare_stream()
-                with for_(*from_array(a, list_param[0])):
+                with for_(*from_array(a, _vector)):
                     update_frequency("resonator", a)
                     play("readout", "resonator")
                     save(a, a_st)
@@ -167,7 +188,7 @@ def test_from_array(config):
             with program() as prog:
                 a = declare(fixed)
                 a_st = declare_stream()
-                with for_(*from_array(a, list_param[0])):
+                with for_(*from_array(a, _vector)):
                     play("readout" * amp(a), "resonator")
                     save(a, a_st)
                 with stream_processing():
@@ -175,55 +196,28 @@ def test_from_array(config):
             return prog
 
     cfg = deepcopy(config)
-    arange_param_list = [
-        [np.logspace(np.log10(10000), np.log10(4), 100), "int"],
-        [np.logspace(np.log10(4), np.log10(10000), 29), "int"],
-        [np.logspace(np.log10(50), np.log10(12500), 19), "int"],
-        [np.logspace(np.log10(50000), np.log10(33), 72), "int"],
-        [np.logspace(6, 4, 19), "int"],
-        [np.logspace(3, 6, 199), "int"],
-        [np.logspace(-3, 0, 99), "fixed"],
-        [np.logspace(-3.5, -1, 11), "fixed"],
-        [np.logspace(0.5, -0.5, 22), "fixed"],
-        [np.logspace(0.5, -3.5, 21), "fixed"],
-        [np.arange(-7.0547, -2.2141, 0.1015), "fixed"],
-        [np.arange(-0.05, -1, -0.15), "fixed"],
-        [np.arange(-1, 2, 0.0006), "fixed"],
-        [np.arange(-11, -100, -2), "int"],
-        [np.arange(20, 71, 2), "int"],
-        [np.linspace(20, 71, 52), "int"],
-        [np.linspace(0.1, 1, 6), "fixed"],
-        [np.arange(10, 20, 1), "int"],
-        [np.arange(20, 71, 1), "int"],
-        [np.arange(0, 71, 2), "int"],
-        [np.arange(0, 1, 0.1), "fixed"],
-        [np.arange(0, 1, 0.2), "fixed"],
-        [np.arange(0.1, 1, 0.2), "fixed"],
-        # [np.arange(0.00015, 1, 0.0001), "fixed"],
-    ]
+    job = simulate_program_and_return(cfg, prog_maker(vector, qua_type))
+    job.result_handles.wait_for_all_values()
+    a_qua = job.result_handles.get("a").fetch_all()["value"]
+    a_list = vector
+    if (qua_type == "int") and (np.isclose(a_list[1] / a_list[0], a_list[-1] / a_list[-2])):
+        a_list = get_equivalent_log_array(a_list)
 
-    for param in arange_param_list:
-        print(f"Test qua_list with {param}:")
-        job = simulate_program_and_return(cfg, prog_maker(param))
-        job.result_handles.wait_for_all_values()
-        a_qua = job.result_handles.get("a").fetch_all()["value"]
-        a_list = param[0]
-        print(a_qua)
-        if (param[1] == "int") and (np.isclose(a_list[1] / a_list[0], a_list[-1] / a_list[-2])):
-            a_list = get_equivalent_log_array(a_list)
-
-        assert len(a_list) == len(a_qua)
-        assert np.allclose(a_list, a_qua, atol=1e-4)
-    print("PASSED !")
+    assert len(a_list) == len(a_qua)
+    assert np.allclose(a_list, a_qua, atol=1e-4)
 
 
-def test_from_array_log_error(config):
-    def prog_maker(list_param):
-        if list_param[1] == "int":
+@pytest.mark.parametrize(["vector", "qua_type"],
+                         [
+                             [np.logspace(np.log10(4), np.log10(10000), 60), "int"]
+                         ])
+def test_from_array_log_error(config, vector, qua_type):
+    def prog_maker(_vector, _qua_type):
+        if _qua_type == "int":
             with program() as prog:
                 a = declare(int)
                 a_st = declare_stream()
-                with for_(*from_array(a, list_param[0])):
+                with for_(*from_array(a, _vector)):
                     update_frequency("resonator", a)
                     play("readout", "resonator")
                     save(a, a_st)
@@ -234,7 +228,7 @@ def test_from_array_log_error(config):
             with program() as prog:
                 a = declare(fixed)
                 a_st = declare_stream()
-                with for_(*from_array(a, list_param[0])):
+                with for_(*from_array(a, _vector)):
                     play("readout" * amp(a), "resonator")
                     save(a, a_st)
                 with stream_processing():
@@ -242,27 +236,32 @@ def test_from_array_log_error(config):
             return prog
 
     cfg = deepcopy(config)
-    arange_param_list = [
-        [np.logspace(np.log10(4), np.log10(10000), 60), "int"],
-    ]
-    for param in arange_param_list:
-        print(f"Test qua_list with {param}:")
-        try:
-            simulate_program_and_return(cfg, prog_maker(param))
-            raise Exception("Didn't catch error!")
-        except Exception as e:
-            if e.__class__.__name__ == "ValueError":
-                pass
-            else:
-                raise Exception from e
+    try:
+        simulate_program_and_return(cfg, prog_maker(vector, qua_type))
+        raise Exception("Didn't catch error!")
+    except Exception as e:
+        if e.__class__.__name__ == "ValueError":
+            pass
+        else:
+            raise Exception from e
 
 
-def test_qua_linspace(config):
-    def prog_maker(linspace_param):
+@pytest.mark.parametrize(["start", "stop", "N"],
+                         [
+                             [0.1, 1, 5],
+                             [0.1, 0.95, 5],
+                             [-1, 0, 2],
+                             [-1, 1, 2],
+                             [-8, 7, 51],
+                             [-0.1, 0.1, 5000],
+                             [-1, 2, 11],
+                         ])
+def test_qua_linspace(config, start, stop, N):
+    def prog_maker(_start, _stop, _N):
         with program() as prog:
             a = declare(fixed)
             a_st = declare_stream()
-            with for_(*qua_linspace(a, linspace_param[0], linspace_param[1], linspace_param[2])):
+            with for_(*qua_linspace(a, _start, _stop, _N)):
                 play("readout" * amp(a), "resonator")
                 save(a, a_st)
             with stream_processing():
@@ -270,36 +269,29 @@ def test_qua_linspace(config):
         return prog
 
     cfg = deepcopy(config)
+    job = simulate_program_and_return(cfg, prog_maker(start, stop, N))
+    job.result_handles.wait_for_all_values()
+    a_qua = job.result_handles.get("a").fetch_all()["value"]
+    a_list = np.linspace(start, stop, N)
 
-    arange_param_list = [
-        [0.1, 1, 5],
-        [0.1, 0.95, 5],
-        [-1, 0, 2],
-        [-1, 1, 2],
-        [-8, 7, 51],
-        [-0.1, 0.1, 5000],
-        [-1, 2, 11],
-    ]
-
-    for param in arange_param_list:
-        print(f"Test qua_arange with {param}:")
-        job = simulate_program_and_return(cfg, prog_maker(param))
-        job.result_handles.wait_for_all_values()
-        a_qua = job.result_handles.get("a").fetch_all()["value"]
-        a_list = np.linspace(param[0], param[1], param[2])
-        print(a_list)
-        print(a_qua)
-        print(a_list - a_qua)
-        assert len(a_list) == len(a_qua)
-        assert np.allclose(a_list, a_qua, atol=1e-4)
+    assert len(a_list) == len(a_qua)
+    assert np.allclose(a_list, a_qua, atol=1e-4)
 
 
-def test_qua_logspace_fixed(config):
-    def prog_maker(logspace_param):
+@pytest.mark.parametrize(["start", "stop", "N"],
+                         [
+                             [0.5, -2, 24],
+                             [0, -3, 51],
+                             [0, -3, 50],
+                             [-4, 0.5, 11],
+                             [-3.8, 0.2, 7],
+                         ])
+def test_qua_logspace_fixed(config, start, stop, N):
+    def prog_maker(_start, _stop, _N):
         with program() as prog:
             a = declare(fixed)
             a_st = declare_stream()
-            with for_(*qua_logspace(a, logspace_param[0], logspace_param[1], logspace_param[2])):
+            with for_(*qua_logspace(a, _start, _stop, _N)):
                 play("readout" * amp(a), "resonator")
                 save(a, a_st)
             with stream_processing():
@@ -307,30 +299,27 @@ def test_qua_logspace_fixed(config):
         return prog
 
     cfg = deepcopy(config)
+    job = simulate_program_and_return(cfg, prog_maker(start, stop, N))
+    job.result_handles.wait_for_all_values()
+    a_qua = job.result_handles.get("a").fetch_all()["value"]
+    a_list = np.logspace(start, stop, N)
 
-    arange_param_list = [
-        [0.5, -2, 24],
-        [0, -3, 51],
-        [0, -3, 50],
-        [-4, 0.5, 11],
-        [-3.8, 0.2, 7],
-    ]
-    for param in arange_param_list:
-        print(f"Test qua_logspace with {param}:")
-        job = simulate_program_and_return(cfg, prog_maker(param))
-        job.result_handles.wait_for_all_values()
-        a_qua = job.result_handles.get("a").fetch_all()["value"]
-        a_list = np.logspace(param[0], param[1], param[2])
-        assert len(a_list) == len(a_qua)
-        assert np.allclose(a_list, a_qua, atol=1e-4)
+    assert len(a_list) == len(a_qua)
+    assert np.allclose(a_list, a_qua, atol=1e-4)
 
 
-def test_qua_logspace_int(config):
-    def prog_maker(logspace_param):
+@pytest.mark.parametrize(["start", "stop", "N"],
+                         [
+                             [np.log10(500), np.log10(12500), 11],
+                             [np.log10(5000), np.log10(125), 30],
+                             [np.log10(40), np.log10(1001), 51],
+                         ])
+def test_qua_logspace_int(config, start, stop, N):
+    def prog_maker(_start, _stop, _N):
         with program() as prog:
             t = declare(int)
             t_st = declare_stream()
-            with for_(*qua_logspace(t, logspace_param[0], logspace_param[1], logspace_param[2])):
+            with for_(*qua_logspace(t, _start, _stop, _N)):
                 play("readout", "resonator", duration=t)
                 save(t, t_st)
             with stream_processing():
@@ -338,28 +327,25 @@ def test_qua_logspace_int(config):
         return prog
 
     cfg = deepcopy(config)
+    job = simulate_program_and_return(cfg, prog_maker(start, stop, N))
+    job.result_handles.wait_for_all_values()
+    a_qua = job.result_handles.get("a").fetch_all()["value"]
+    a_list = get_equivalent_log_array(np.round(np.logspace(start, stop, N)))
 
-    arange_param_list = [
-        [np.log10(500), np.log10(12500), 11],
-        [np.log10(5000), np.log10(125), 30],
-        [np.log10(40), np.log10(1001), 51],
-    ]
-    for param in arange_param_list:
-        print(f"Test qua_logspace with {param}:")
-        job = simulate_program_and_return(cfg, prog_maker(param))
-        job.result_handles.wait_for_all_values()
-        a_qua = job.result_handles.get("a").fetch_all()["value"]
-        a_list = get_equivalent_log_array(np.round(np.logspace(param[0], param[1], param[2])))
-        assert len(a_list) == len(a_qua)
-        assert np.allclose(a_list, a_qua, atol=1e-4)
+    assert len(a_list) == len(a_qua)
+    assert np.allclose(a_list, a_qua, atol=1e-4)
 
 
-def test_qua_logspace_error(config):
-    def prog_maker(logspace_param):
+@pytest.mark.parametrize(["start", "stop", "N"],
+                         [
+                             [np.log10(4), np.log10(10000), 60],
+                         ])
+def test_qua_logspace_error(config, start, stop, N):
+    def prog_maker(_start, _stop, _N):
         with program() as prog:
             t = declare(int)
             t_st = declare_stream()
-            with for_(*qua_logspace(t, logspace_param[0], logspace_param[1], logspace_param[2])):
+            with for_(*qua_logspace(t, _start, _stop, _N)):
                 play("readout", "resonator", duration=t)
                 save(t, t_st)
             with stream_processing():
@@ -367,16 +353,11 @@ def test_qua_logspace_error(config):
         return prog
 
     cfg = deepcopy(config)
-    arange_param_list = [
-        [np.log10(4), np.log10(10000), 60],
-    ]
-    for param in arange_param_list:
-        print(f"Test qua_list with {param}:")
-        try:
-            simulate_program_and_return(cfg, prog_maker(param))
-            raise Exception("Didn't catch error!")
-        except Exception as e:
-            if e.__class__.__name__ == "ValueError":
-                pass
-            else:
-                raise Exception from e
+    try:
+        simulate_program_and_return(cfg, prog_maker(start, stop, N))
+        raise Exception("Didn't catch error!")
+    except Exception as e:
+        if e.__class__.__name__ == "ValueError":
+            pass
+        else:
+            raise Exception from e
