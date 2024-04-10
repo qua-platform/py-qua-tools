4907b1c46f73fbc8ae873ef8ad6292864025f998
Dean Poulos <dean.poulos7@gmail.com>
2024-04-10 19:07:33 +1000

    Add server tests for a variety of cases as well as example qm_config.toml file.

--- qualang_tools/macros/long_wait.py
+++ qualang_tools/macros/long_wait.py
@@ -1,29 +1,45 @@
 from typing import Union
+
+import numpy as np
 from qm.qua import declare, wait, for_
+from qualang_tools.addons.calibration.calibrations import u
 
 MAX_WAIT = 2**31 - 1  # units of clock cycles (4ns)
 
 
-def long_wait(wait_time: Union[float, int], *elements: str):
+def long_wait(wait_time: Union[float, int], *elements: str, threshold_for_looping: int = u.to_clock_cycles(1e6)):
     """Perform a QUA wait command for longer than the current maximum wait time, 2^31-1.
 
     This macro unrolls into QUA for-loop of smaller waits if the maximum wait time is exceeded in `wait_time`.
 
-    Note: `wait_time` must strictly be a python literal.
+    Note: `wait_time` must be a python number.
 
     Args:
         wait_time (Union[float, int]): Desired wait time in units of clock cycles (4ns).
         *elements (str): Variable length argument list of elements to wait on.
+        threshold_for_looping: Minimum wait time at which `long_wait` uses a QUA for-loop of waits (default 1ms)
     """
     i = declare(int)
 
-    if not isinstance(wait_time, (float, int)):
-        raise TypeError(f"Expected wait_time to be a float or an integer, got: {type(wait_time)}.")
+    if not isinstance(wait_time, (int, np.integer)):
+        raise TypeError(f"Expected wait_time to be a float or an integer, got {type(wait_time)}.")
 
-    if wait_time < MAX_WAIT:
+    if threshold_for_looping < 4:
+        raise ValueError(f"Expected loop threshold to be greater than 4 clock cycles, got {threshold_for_looping}")
+
+    if threshold_for_looping > MAX_WAIT:
+        raise ValueError(f"Expected loop threshold to be less than the maximum wait time "
+                         f"{MAX_WAIT}, got {threshold_for_looping}")
+
+    if wait_time < threshold_for_looping:
         wait(wait_time, *elements)
+
     else:
-        loop_runs = (wait_time // MAX_WAIT) - 1
+        loop_runs = (wait_time // threshold_for_looping) - 1
+
+        # wait for N x threshold wait time
         with for_(i, 0, i < loop_runs, i + 1):
-            wait(MAX_WAIT, *elements)
-        wait(int(wait_time - MAX_WAIT * loop_runs), *elements)
+            wait(threshold_for_looping, *elements)
+
+        # wait for the remainder
+        wait(int(wait_time - threshold_for_looping * loop_runs), *elements)
--- tests_against_server/test_long_wait.py
+++ tests_against_server/test_long_wait.py
@@ -1,94 +1,98 @@
-import numpy as np
+import time
+
 import pytest
+from qualang_tools.addons.calibration.calibrations import u
 from qualang_tools.macros.long_wait import long_wait, MAX_WAIT
 from qm.qua import *
-from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm import SimulationConfig, LoopbackInterface
 
-import matplotlib.pyplot as plt
 
-@pytest.fixture
-def config():
-    return {
-        "version": 1,
-        "controllers": {
-            "con1": {
-                "analog_outputs": {1: {"offset": 0.0}},
-                "digital_outputs": {},
-                "analog_inputs": {
-                    1: {"offset": 0.0, "gain_db": 0},  # I from down-conversion
-                    2: {"offset": 0.0, "gain_db": 0},  # Q from down-conversion
-                },
-            }
-        },
-        "elements": {
-            "resonator": {
-                "singleInput": {
-                    "port": ("con1", 1),
-                },
-                "intermediate_frequency": 100e6,
-                "operations": {
-                    "readout": "readout_pulse",
-                },
-                "outputs": {
-                    "out1": ("con1", 1),
-                    "out2": ("con1", 2),
-                },
-                "time_of_flight": 24,
-                "smearing": 0,
-            },
-        },
-        "pulses": {
-            "readout_pulse": {
-                "operation": "measurement",
-                "length": 80,
-                "waveforms": {
-                    "single": "const_wf",
-                },
-                "integration_weights": {
-                    "cos": "cosine_weights",
-                    "sin": "sine_weights",
-                    "minus_sin": "minus_sine_weights",
-                },
-                "digital_marker": "ON",
-            },
-        },
-        "waveforms": {
-            "zero_wf": {"type": "constant", "sample": 0.0},
-            "const_wf": {"type": "constant", "sample": 0.2},
-        },
-        "digital_waveforms": {
-            "ON": {"samples": [(1, 0)]},
-        },
-    }
+def test_long_wait_time_live(qmm, config):
+    """ Tests that the program runs and finishes without crashing. """
+    op = "playOp"
 
+    wait_time = u.to_clock_cycles(30e9)  # 50s
+    assert wait_time > MAX_WAIT  # to prove this is truly a long wait
 
-def simulate_program_and_return(config, prog, duration=50000):
-    qmm = QuantumMachinesManager(host="172.16.33.101", cluster_name="Cluster_83")
-    qmm.close_all_quantum_machines()
-    job = qmm.simulate(
-        config,
-        prog,
-        SimulationConfig(
-            duration,
-            simulation_interface=LoopbackInterface([("con1", 1, "con1", 1)]),
-            include_analog_waveforms=True,
-        ),
-    )
-
-    # qm = qmm.open_qm(config)
-    # job = qm.execute(prog)
-    return job
-
-
-# @pytest.mark.parametrize("wait_time", [4, 16, MAX_WAIT - 1, MAX_WAIT, MAX_WAIT + 1, 1000*MAX_WAIT])
-@pytest.mark.parametrize("wait_time", [100])
-def test_long_wait_time(config, wait_time):
     with program() as prog:
-        play("pulse", "element")
+        play(op, "qe1")  # very short pulse
         long_wait(wait_time)
-        play("pulse", "element")
 
-    job = simulate_program_and_return(config, prog)
-    job.get_simulated_samples().con1.plot()
-    plt.show()
+    job = execute_program(qmm, config, prog, simulate=False)
+
+    runtime = 0
+    is_running = True
+
+    # calculate runtime
+    while is_running:
+        start = time.time()
+        is_running = job._is_job_running()
+        end = time.time()
+
+        # count every 2s, accounting for the call time of _is_job_running()
+        call_time = end - start
+        runtime += 2
+        time.sleep(2 - call_time)
+
+    assert runtime / 4 == wait_time / 1e9
+
+
+# small wait threshold to verify behaviour at MAX_WAIT without having to simulate that far
+dummy_max_wait_time = 200  # clock cycles
+
+
+@pytest.mark.parametrize("wait_time", [4, 16, 100,
+                                       dummy_max_wait_time-1,
+                                       dummy_max_wait_time,
+                                       dummy_max_wait_time+1,
+                                       100*dummy_max_wait_time])
+def test_long_wait_time_simulation(qmm, config, wait_time):
+    """ Extracts the wait time from simulation and asserts it to be equal to the input wait time."""
+    op = "playOp"
+
+    with program() as prog:
+        play(op, "qe1")
+        long_wait(wait_time, threshold_for_looping=dummy_max_wait_time)
+        play(op, "qe1")
+
+    element = config['elements']['qe1']
+    pulse = config['pulses'][element['operations'][op]]
+    pulse_length = pulse['length']
+    pulse_amplitude = config['waveforms'][pulse['waveforms']['single']]['sample']
+
+    job = execute_program(qmm, config, prog, simulate=True, simulation_duration=wait_time + 2*pulse_length + 100)
+
+    output = job.get_simulated_samples().con1.analog['1-1']
+
+    th = pulse_amplitude / 2  # edge threshold
+    rising_edges = np.where((output[:-1] <= th/2) & (output[1:] > th/2))[0]
+    falling_edges = np.where((output[:-1] > th/2) & (output[1:] <= th/2))[0]
+
+    # in a square -> wait -> square program, the wait time lies between the first falling edge
+    # and the second rising edge of the entire program.
+    wait_samples = output[falling_edges[0]:rising_edges[1]]
+
+    # plt.plot(output)
+    # plt.axvline(falling_edges[0], linestyle='--')
+    # plt.axvline(rising_edges[1], linestyle='--')
+    # plt.show()
+
+    assert len(wait_samples) == wait_time * u.clock_cycle
+
+
+def execute_program(qmm, config, prog, simulate=True, simulation_duration=10_000):
+    if simulate:
+        job = qmm.simulate(
+            config,
+            prog,
+            SimulationConfig(
+                simulation_duration,
+                simulation_interface=LoopbackInterface([("con1", 1, "con1", 1)]),
+                include_analog_waveforms=True,
+            ),
+        )
+    else:
+        qm = qmm.open_qm(config)
+        job = qm.execute(prog)
+
+    return job
