b4cfc188204cb9d133653c51e37c80179f724478
Dean Poulos <dean.poulos7@gmail.com>
2024-04-29 01:56:29 +1000

    Perform int checks for  so final wait time is always int.

--- qualang_tools/macros/long_wait.py
+++ qualang_tools/macros/long_wait.py
@@ -2,12 +2,14 @@ from typing import Union
 
 import numpy as np
 from qm.qua import declare, wait, for_
-from qualang_tools.addons.calibration.calibrations import u
+
+from qualang_tools.units import unit
 
 MAX_WAIT = 2**31 - 1  # units of clock cycles (4ns)
 
 
-def long_wait(wait_time: Union[int, np.integer], *elements: str, threshold_for_looping: int = u.to_clock_cycles(1e6)):
+def long_wait(wait_time: Union[int, np.integer], *elements: str,
+              threshold_for_looping: int = unit(coerce_to_integer=True).to_clock_cycles(1_000_000)):
     """Perform a QUA wait command for longer than the current maximum wait time, 2^31-1.
 
     This macro unrolls into QUA for-loop of smaller waits if the maximum wait time is exceeded in `wait_time`.
@@ -22,7 +24,10 @@ def long_wait(wait_time: Union[int, np.integer], *elements: str, threshold_for_l
          times. In units of clock cycles.
     """
     if not isinstance(wait_time, (int, np.integer)):
-        raise TypeError(f"Expected wait_time to be a float or an integer, got {type(wait_time)}.")
+        raise TypeError(f"Expected `wait_time` to be a float or an integer, got {type(wait_time)}.")
+
+    if not isinstance(threshold_for_looping, int):
+        raise TypeError(f"Expected `threshold_for_looping` to be a an integer, got {type(threshold_for_looping)}.")
 
     if threshold_for_looping < 4:
         raise ValueError(f"Expected loop threshold to be greater than 4 clock cycles, got {threshold_for_looping}")
