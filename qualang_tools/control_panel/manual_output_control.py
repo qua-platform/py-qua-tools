qua-platform/py-qua-tools

commit 798e44d28b91561fe7f385b0d7e57a0127c86e33

control_panel - Fix rounding error that caused voltage drifts (#211)
* control_panel - Fix rounding error that coused voltage drifts

* typo

* another typo

* round amplitude value as in PP

* made them separate functions to remove if

* restored EOL

* format

---------

Co-authored-by: Yoav Romach <53370976+yomach@users.noreply.github.com>
Co-authored-by: Yoav Romach <yomach@gmail.com>

qualang_tools/control_panel/manual_output_control.py
@@ -1,6 +1,7 @@
 """calling function libraries"""
 
 import copy
+import math
 from time import sleep
 
 import numpy as np
@@ -11,7 +12,11 @@ from qm.qua import *
 
 
 def _round_to_fixed_point_accuracy(x, accuracy=2**-16):
-    return np.round(x / accuracy) * accuracy
+    return round(x / accuracy) * accuracy
+
+
+def _floor_to_fixed_point_accuracy(x, accuracy=2**-16):
+    return math.floor(x / accuracy) * accuracy
 
 
 class ManualOutputControl:
@@ -242,19 +247,23 @@ class ManualOutputControl:
                 raise Exception(f"The absolute value of the amplitude must smaller than 0.5, {value} was given")
 
         prev_value = self.analog_data[element]["amplitude"]
-        self.analog_data[element]["amplitude"] = value
         if value != 0:
-            value = (value - prev_value) * (1 / self.ANALOG_WAVEFORM_AMPLITUDE)
-            value = _round_to_fixed_point_accuracy(value)
-            if value == 0:
+            delta_value = (value - prev_value) * (1 / self.ANALOG_WAVEFORM_AMPLITUDE)
+            delta_value = _round_to_fixed_point_accuracy(delta_value)
+            if delta_value == 0:
                 return
+        else:
+            delta_value = value
+        self.analog_data[element]["amplitude"] = _floor_to_fixed_point_accuracy(
+            prev_value + delta_value * self.ANALOG_WAVEFORM_AMPLITUDE
+        )
 
         while not self.analog_job.is_paused():
             sleep(0.01)
 
         self.analog_qm.set_io_values(
             int(self.analog_elements.index(element)) + len(self.analog_elements),
-            float(value),
+            float(delta_value),
         )
         self.analog_job.resume()
 

commit a61bb414b445da192cc73b9b2f9ed49511ebad78

Fix voltage book-keeping error introduced in #211 (#220)
* Fix voltage booking error introduced in the previous fix.

* Change variables to be explicit

* remove special 0 case for ramp_to_zero

---------

Co-authored-by: Yoav Romach <53370976+yomach@users.noreply.github.com>
Co-authored-by: Yoav Romach <yomach@gmail.com>

qualang_tools/control_panel/manual_output_control.py
@@ -247,13 +247,10 @@ class ManualOutputControl:
                 raise Exception(f"The absolute value of the amplitude must smaller than 0.5, {value} was given")
 
         prev_value = self.analog_data[element]["amplitude"]
-        if value != 0:
-            delta_value = (value - prev_value) * (1 / self.ANALOG_WAVEFORM_AMPLITUDE)
-            delta_value = _round_to_fixed_point_accuracy(delta_value)
-            if delta_value == 0:
-                return
-        else:
-            delta_value = value
+        delta_value = (value - prev_value) * (1 / self.ANALOG_WAVEFORM_AMPLITUDE)
+        delta_value = _round_to_fixed_point_accuracy(delta_value)
+        if delta_value == 0:
+            return
         self.analog_data[element]["amplitude"] = _floor_to_fixed_point_accuracy(
             prev_value + delta_value * self.ANALOG_WAVEFORM_AMPLITUDE
         )
@@ -618,10 +615,7 @@ class ManualOutputControl:
             with switch_(input1):
                 for i in range(len(self.analog_elements)):
                     with case_(i):
-                        with if_(a == 0):
-                            ramp_to_zero(self.analog_elements[i], 1)
-                        with else_():
-                            play("play" * amp(a), self.analog_elements[i])
+                        play("play" * amp(a), self.analog_elements[i])
         with else_():
             freq = declare(int)
             assign(freq, input2)