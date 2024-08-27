08ccd32d212f5f1b155cd88d54a5229510c4033a
Dean Poulos <dean.poulos7@gmail.com>
2024-08-27 16:30:12 +1000

    Add some constants.

--- qualang_tools/wirer/visualizer/constants.py
+++ qualang_tools/wirer/visualizer/constants.py
@@ -13,17 +13,21 @@ INSTRUMENT_FIGURE_DIMENSIONS = {
     "Octave": {"width": 3, "height": 1}
 }
 
+OPX_PLUS_ASPECT = (INSTRUMENT_FIGURE_DIMENSIONS["OPX+"]["height"] /
+                   INSTRUMENT_FIGURE_DIMENSIONS["OPX+"]["width"])
+OPX_1000_ASPECT = (INSTRUMENT_FIGURE_DIMENSIONS["OPX1000"]["height"] /
+                   INSTRUMENT_FIGURE_DIMENSIONS["OPX1000"]["width"])
 # Define the port positions for different modules
 PORT_SPACING_FACTOR = 0.1
 PORT_SIZE = 0.04
 PORT_POSITIONS = {
     "lf-fem": {
-        "output": [(0.05 + 0.25/8*3, 1 - i * PORT_SPACING_FACTOR) for i in range(8)],
-        "input": [(0.05 + 0.75/8*3, 1 - (6+i) * PORT_SPACING_FACTOR) for i in range(2)],
+        "output": [(0.05 + 0.25 * OPX_1000_ASPECT, 1 - i * PORT_SPACING_FACTOR) for i in range(8)],
+        "input": [(0.05 + 0.75 * OPX_1000_ASPECT, 1 - (6+i) * PORT_SPACING_FACTOR) for i in range(2)],
     },
     "mw-fem": {
-        "output": [(0.05 + 0.25/8*3, 1 - i * PORT_SPACING_FACTOR) for i in range(8)],
-        "input": [(0.05 + 0.75/8*3, 1 - i * PORT_SPACING_FACTOR * 7) for i in range(2)],
+        "output": [(0.05 + 0.25 * OPX_1000_ASPECT, 1 - i * PORT_SPACING_FACTOR) for i in range(8)],
+        "input": [(0.05 + 0.75 * OPX_1000_ASPECT, 1 - i * PORT_SPACING_FACTOR * 7) for i in range(2)],
     },
     "opx+": {
         "input": [(0.75, 1 - i * PORT_SPACING_FACTOR * 2) for i in range(2)],
