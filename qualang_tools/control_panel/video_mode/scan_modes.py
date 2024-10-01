96689aaade861e2fb414490bc7d9f2f733b7fd00
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-10-01 16:01:17 +0200

    add switch raster scan

--- qualang_tools/control_panel/video_mode/scan_modes.py
+++ qualang_tools/control_panel/video_mode/scan_modes.py
@@ -52,6 +52,108 @@ class RasterScan(ScanMode):
                 yield voltages
 
 
+class SwitchRasterScan(ScanMode):
+
+    @staticmethod
+    def interleave_arr(arr: np.ndarray) -> np.ndarray:
+        mid_idx = (len(arr) + 1) // 2
+        arr1 = arr[:mid_idx]
+        arr2 = arr[mid_idx:]
+
+        interleaved = [elem for pair in zip(arr1, arr2) for elem in pair]
+        if len(arr1) > len(arr2):
+            interleaved.extend(arr1[len(arr2) :])
+        elif len(arr2) > len(arr1):
+            interleaved.extend(arr2[len(arr1) :])
+        return np.array(interleaved)
+
+    def get_idxs(self, x_points: int, y_points: int) -> Tuple[np.ndarray, np.ndarray]:
+        y_idxs = self.interleave_arr(np.arange(y_points))
+        x_idxs = np.tile(np.arange(x_points), y_points)
+        y_idxs = np.repeat(y_idxs, x_points)
+        return x_idxs, y_idxs
+
+    def scan(self, x_vals: Sequence[float], y_vals: Sequence[float]):
+        voltages = {"x": declare(fixed), "y": declare(fixed)}
+
+        with for_(*from_array(voltages["y"], self.interleave_arr(y_vals))):  # type: ignore
+            with for_(*from_array(voltages["x"], x_vals)):  # type: ignore
+                yield voltages
+
+
+class SwitchRaster(ScanMode):
+    def get_idxs(self, x_points: int, y_points: int) -> Tuple[np.ndarray, np.ndarray]:
+        assert x_points == y_points, "Spiral only works for square grids"
+
+        num_half_spirals = x_points
+        x_idx = x_points // 2
+        y_idx = y_points // 2
+
+        idxs_x = [x_idx]
+        idxs_y = [y_idx]
+
+        for half_spiral_idx in range(num_half_spirals):
+            initial_direction_RL = "L" if half_spiral_idx % 2 else "R"
+            direction_UD = "U" if half_spiral_idx % 2 else "D"
+            direction_LR = "R" if half_spiral_idx % 2 else "L"
+
+            if half_spiral_idx:
+                x_idx += 1 if initial_direction_RL == "R" else -1
+                idxs_x.append(x_idx)
+                idxs_y.append(y_idx)
+
+            for _ in range(half_spiral_idx):
+                y_idx += 1 if direction_UD == "U" else -1
+                idxs_x.append(x_idx)
+                idxs_y.append(y_idx)
+
+            for _ in range(half_spiral_idx):
+                x_idx += 1 if direction_LR == "R" else -1
+                idxs_x.append(x_idx)
+                idxs_y.append(y_idx)
+
+        return np.array(idxs_x), np.array(idxs_y)
+
+    def scan(self, x_vals: Sequence[float], y_vals: Sequence[float]):
+        movement_direction = declare(fixed)
+        half_spiral_idx = declare(int)
+        k = declare(int)
+        x = declare(fixed)
+        y = declare(fixed)
+        voltages = {"x": x, "y": y}
+
+        assert len(x_vals) == len(
+            y_vals
+        ), f"x_vals and y_vals must have the same length ({len(x_vals)} != {len(y_vals)})"
+        num_half_spirals = len(x_vals)
+        x_step = x_vals[1] - x_vals[0]
+        y_step = y_vals[1] - y_vals[0]
+
+        assign(movement_direction, -1.0)
+        assign(x, 0.0)
+        assign(y, 0.0)
+        yield voltages
+
+        with for_(half_spiral_idx, 0, half_spiral_idx < num_half_spirals, half_spiral_idx + 1):  # type: ignore
+            # First take one step in the opposite XY direction
+            with if_(half_spiral_idx > 0):
+                assign(x, x - x_step * movement_direction)
+                yield voltages
+
+            with for_(k, 0, k < half_spiral_idx, k + 1):  # type: ignore
+                assign(y, y + y_step * movement_direction)
+                yield voltages
+
+            with for_(k, 0, k < half_spiral_idx, k + 1):  # type: ignore
+                assign(x, x + x_step * movement_direction)
+                yield voltages
+
+            assign(movement_direction, -movement_direction)
+
+        assign(x, 0)
+        assign(y, 0)
+
+
 class SpiralScan(ScanMode):
     def get_idxs(self, x_points: int, y_points: int) -> Tuple[np.ndarray, np.ndarray]:
         assert x_points == y_points, "Spiral only works for square grids"
