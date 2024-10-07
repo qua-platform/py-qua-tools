e2f14376109be935d151315e7ad97d9d68f1d034
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-10-07 19:16:16 +0200

    add sweep_axis docstring

--- qualang_tools/control_panel/video_mode/sweep_axis.py
+++ qualang_tools/control_panel/video_mode/sweep_axis.py
@@ -11,6 +11,17 @@ __all__ = ["SweepAxis"]
 
 @dataclass
 class SweepAxis:
+    """Class representing a sweep axis.
+
+    Attributes:
+        name: Name of the axis.
+        span: Span of the axis.
+        points: Number of points in the sweep.
+        label: Label of the axis.
+        offset_parameter: Offset parameter of the axis.
+        attenuation: Attenuation of the axis (0 by default)
+    """
+
     name: str
     span: float
     points: int
@@ -20,18 +31,22 @@ class SweepAxis:
 
     @property
     def sweep_values(self):
+        """Returns axis sweep values using span and points."""
         return np.linspace(-self.span / 2, self.span / 2, self.points)
 
     @property
     def sweep_values_unattenuated(self):
+        """Returns axis sweep values without attenuation."""
         return self.sweep_values * 10 ** (self.attenuation / 20)
 
     @property
     def sweep_values_with_offset(self):
+        """Returns axis sweep values with offset."""
         if self.offset_parameter is None:
             return self.sweep_values_unattenuated
         return self.sweep_values_unattenuated + self.offset_parameter.get_latest()
 
     @property
-    def amplitude_scale(self):
+    def scale(self):
+        """Returns axis scale factor, calculated from attenuation."""
         return 10 ** (-self.attenuation / 20)
