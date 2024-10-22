e0afe4009390f6e0282a9a7628ea42f18b96873f
paulQM <paul.jamet@quantum-machines.co>
2024-10-22 15:27:56 +0200

    add calibration result plotting functions

--- qualang_tools/octave_tools/__init__.py
+++ qualang_tools/octave_tools/__init__.py
@@ -1,8 +1,9 @@
+from qualang_tools.octave_tools.calibration_result_plotter import show_if_result, show_lo_result
 from qualang_tools.octave_tools.octave_tools import (
     get_calibration_parameters_from_db,
-    set_correction_parameters_to_opx,
     get_correction_for_each_LO_and_IF,
     octave_calibration_tool,
+    set_correction_parameters_to_opx,
 )
 
 __all__ = [
@@ -10,4 +11,6 @@ __all__ = [
     "set_correction_parameters_to_opx",
     "get_correction_for_each_LO_and_IF",
     "octave_calibration_tool",
+    "show_lo_result",
+    "show_if_result",
 ]
