Fix bugs with fitting when leakage is present.

qualang_tools/characterization/two_qubit_rb/RBResult.py
@@ -3,6 +3,7 @@ from typing import Tuple, Optional, List
 
 import numpy as np
 import xarray as xr
+from matplotlib.figure import Figure
 
 from qualang_tools.characterization.two_qubit_rb.analysis.fitting import (
     fit_to_single_exponential,
@@ -87,11 +88,18 @@ class RBResult:
         probability_of_ground_state, _ = self.probability_of_ground_state()
 
         if self.leakage_was_measured:
-            leakage_fit = fit_to_single_exponential(self.circuit_depths, probability_of_remaining_in_computational_subspace)
-            ground_state_fit = fit_to_double_exponential(self.circuit_depths, probability_of_ground_state, lambda_1=leakage_fit.lambda_1, p0=p0)
+            leakage_fit = fit_to_single_exponential(
+                self.circuit_depths, probability_of_remaining_in_computational_subspace
+            )
+            ground_state_fit = fit_to_double_exponential(
+                self.circuit_depths, probability_of_ground_state, lambda_1=leakage_fit.lambda_2, p0=p0,
+                bounds=[(0, 0, 0, 0), (1, 1, 1, 1)]
+            )
 
         else:
-            ground_state_fit = fit_to_single_exponential(self.circuit_depths, probability_of_ground_state, p0=p0)
+            ground_state_fit = fit_to_single_exponential(
+                self.circuit_depths, probability_of_ground_state, p0=p0
+            )
 
         return TwoQubitRbFit(
             ground_state_fit=ground_state_fit,
@@ -126,22 +134,21 @@ class RBResult:
                 computational subspace, i.e., the probability of measuring |00>, |01>, |10>, or |11>.
                 xarray.DataArray: The standard deviation of the probability along the randomization axis.
         """
-        individual_computational_subspace_states = [0, 1]
-        computational_subspace_state = self.state.isin(individual_computational_subspace_states).all(dim="qubit")
+        computational_subspace_state = (self.state < 2).all(dim="qubit")
         computational_subspace_state_probability = computational_subspace_state.mean(("repeat", "average"))
         computational_subspace_state_probability_err = computational_subspace_state.mean("average").std("repeat")
 
         return computational_subspace_state_probability, computational_subspace_state_probability_err
 
 
-    def plot(self, fit: Optional[TwoQubitRbFit] = None):
+    def plot(self, fit: Optional[TwoQubitRbFit] = None) -> Figure:
         if fit is None:
             fit = self.fit()
 
         y_data, _ = self.probability_of_ground_state()
         fidelity = self.get_fidelity(fit)
         x_fit, y_fit = fit.ground_state_fit.sample(self.circuit_depths)
-        plot_results(
+        fig = plot_results(
             x_data=self.circuit_depths,
             y_data=y_data.data,
             y_err_data=(self.data == 0).all(dim="qubit").mean(dim="average").std(dim="repeat").state.data,
@@ -150,7 +157,9 @@ class RBResult:
             fidelity=fidelity
         )
 
-    def plot_two_qubit_state_distribution(self):
+        return fig
+
+    def plot_two_qubit_state_distribution(self) -> Figure:
         """
         Plot how the two-qubit state is distributed as a function of circuit-depth on average.
         """

qualang_tools/characterization/two_qubit_rb/analysis/fitting.py
@@ -16,9 +16,9 @@ from scipy.optimize import curve_fit
 class DoubleExponentialInitialGuess:
     A: Optional[float] = 0.25
     B: Optional[float] = 0.75
-    lambda_1: Optional[float] = 0.9
-    C: Optional[float] = 0.8
-    lambda_2: Optional[float] = 0.9
+    lambda_1: Optional[float] = 0.7
+    C: Optional[float] = 0.75
+    lambda_2: Optional[float] = 0.7
 
 
 # default initial guess
@@ -72,11 +72,11 @@ class DoubleExponentialFit:
 
 @dataclass
 class TwoQubitRbFit:
-    ground_state_fit: Optional[DoubleExponentialFit]
-    leakage_fit: Optional[DoubleExponentialFit]
+    ground_state_fit: DoubleExponentialFit
+    leakage_fit: Optional[DoubleExponentialFit] = None
 
 
-def fit_to_single_exponential(circuit_depths, data: xr.DataArray,
+def fit_to_single_exponential(circuit_depths: np.ndarray, data: xr.DataArray,
                               p0: Optional[DoubleExponentialInitialGuess] = None) -> DoubleExponentialFit:
     """
     Fits the RB data to the single-exponential model A + Bλ₁ᵐ.
@@ -95,7 +95,7 @@ def fit_to_single_exponential(circuit_depths, data: xr.DataArray,
         xdata=circuit_depths,
         ydata=data,
         p0=p0,
-        maxfev=10000
+        maxfev=20000
     )
 
     perr = np.sqrt(np.diag(pcov))
@@ -106,8 +106,8 @@ def fit_to_single_exponential(circuit_depths, data: xr.DataArray,
         lambda_2=popt[2], lambda_2_err=perr[2]
     )
 
-def fit_to_double_exponential(data: xr.DataArray, lambda_1: Optional[float] = None,
-                              p0: Optional[DoubleExponentialInitialGuess] = None) -> DoubleExponentialFit:
+def fit_to_double_exponential(circuit_depths: np.ndarray, data: xr.DataArray, lambda_1: Optional[float] = None,
+                              p0: Optional[DoubleExponentialInitialGuess] = None, bounds = None) -> DoubleExponentialFit:
     """
     Fits the RB data to the double-exponential model A + Bλ₁ᵐ + Cλ₂ᵐ.
 
@@ -120,7 +120,7 @@ def fit_to_double_exponential(data: xr.DataArray, lambda_1: Optional[float] = No
     """
 
     if lambda_1 is not None:
-        f = functools.partial(double_exponential_decay_model, lambda_1=lambda_1)
+        f = lambda m, A, B, C, lambda_2: double_exponential_decay_model(m, A, B, lambda_1, C, lambda_2)
         if p0 is None:
             p0 = [_p0.A, _p0.B, _p0.C, _p0.lambda_2]
     else:
@@ -130,10 +130,11 @@ def fit_to_double_exponential(data: xr.DataArray, lambda_1: Optional[float] = No
 
     popt, pcov = curve_fit(
         f=f,
-        xdata=data.circuit_depths,
+        xdata=circuit_depths,
         ydata=data,
         p0=p0,
-        maxfev=10000
+        maxfev=10000,
+        bounds=bounds
     )
 
     perr = np.sqrt(np.diag(pcov))  # Extract standard errors