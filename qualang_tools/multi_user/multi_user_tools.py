bb1bf4a1df89410cd252c5cccc3af430f387be58
TheoQM <theo@quantum-machines.co>
2025-05-12 17:50:41 +0200

    more or less working solution

--- qualang_tools/multi_user/multi_user_tools.py
+++ qualang_tools/multi_user/multi_user_tools.py
@@ -3,18 +3,24 @@ from contextlib import contextmanager
 from time import sleep, time
 from typing import Generator
 
-from qm import QuantumMachine
-from qm import QuantumMachinesManager
+from qm import QuantumMachinesManager, QuantumMachine, QopCaps
+from qm.logging_utils import set_logging_level
 
 msg = (
     "A quantum machine cannot be opened because an existing quantum machine, using the same ports, is currently "
     "running a program. Please close the currently open quantum machine."
 )
+msg_opx1000 = "Resources already locked"
 
 
 class BusyFilter(logging.Filter):
     def filter(self, record):
-        return not ("already uses" in record.getMessage() or msg in record.getMessage())
+        return not (
+            "already uses" in record.getMessage()
+            or msg in record.getMessage()
+            or msg_opx1000 in record.getMessage()
+            or "PHYSICAL CONFIG ERROR" in record.getMessage()
+        )
 
 
 @contextmanager
@@ -35,27 +41,25 @@ def qm_session(qmm: QuantumMachinesManager, config: dict, timeout: int = 100) ->
     """
     if not timeout > 0:
         raise ValueError(f"{timeout=} must be positive")
-    qm_log = logging.getLogger("qm.api.frontend_api")  # formerly "qm"
+
+    qm_log = logging.getLogger("qm.api.frontend_api")  # formerly "qm" & qm.api.frontend_api
     filt = BusyFilter()
     is_busy = True
     printed = False
     t0 = time()
     elapsed_time = 0
+    set_logging_level("ERROR")
     while is_busy and elapsed_time < timeout:
         try:
             qm_log.addFilter(filt)
             qm = qmm.open_qm(config, close_other_machines=False)
+
         except Exception as e:
-            if (
-                hasattr(e, "errors")
-                and isinstance(e.errors, list)
-                and len(e.errors) > 1
-                and isinstance(e.errors[1], tuple)
-                and len(e.errors[1]) > 2
-                and msg in e.errors[1][2]
+            if (qmm.capabilities.supports(QopCaps.qop3) and msg_opx1000 in e.errors[0][2]) or (
+                ~qmm.capabilities.supports(QopCaps.qop3) and msg in e.errors[1][2]
             ):
                 if not printed:
-                    qm_log.warning(f"QOP is busy. Waiting for it to free up for {timeout}s...")
+                    qm_log.error(f"QOP is busy. Waiting for it to free up for {timeout}s...")
                     printed = True
                 sleep(0.2)
                 elapsed_time = time() - t0
@@ -63,16 +67,18 @@ def qm_session(qmm: QuantumMachinesManager, config: dict, timeout: int = 100) ->
                 raise Exception from e
         else:
             is_busy = False
+            set_logging_level("INFO")
             qm_log.info("Opening QM")
         finally:
             qm_log.removeFilter(filt)
+
     if is_busy and elapsed_time >= timeout:
         qm_log.warning(f"While waiting for QOP to free, reached timeout: {timeout}s")
         raise TimeoutError(f"While waiting for QOP to free, reached timeout: {timeout}s")
     try:
         yield qm
-        qop_type = qm.get_config()["controllers"]["type"]
-        if qop_type == "opx1000":
+
+        if qmm.capabilities.supports(QopCaps.qop3):
             while qm.get_jobs(status=["Running"]):
                 sleep(0.2)
         else:
