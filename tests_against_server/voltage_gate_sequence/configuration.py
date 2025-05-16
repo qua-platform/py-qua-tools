e52ace3171c56ac65eb0b381f2663bb0cba30b0f
TheoQM <theo@quantum-machines.co>
2025-05-16 17:26:51 +0200

    add test against server

---
+++ tests_against_server/voltage_gate_sequence/configuration.py
@@ -0,0 +1,340 @@
+import numpy as np
+from qualang_tools.units import unit
+from qualang_tools.voltage_gates import VoltageGateSequence
+
+
+#######################
+# AUXILIARY FUNCTIONS #
+#######################
+u = unit(coerce_to_integer=True)
+
+######################
+# Network parameters #
+######################
+qop_ip = "172.16.33.101"  # Write the QM router IP address
+cluster_name = "Cluster_83"  # Write your cluster_name if version >= QOP220
+qop_port = None  # Write the QOP port if version < QOP220
+
+# Path to save data
+octave_config = None
+
+
+#############################################
+#              OPX PARAMETERS               #
+#############################################
+
+######################
+#       READOUT      #
+######################
+# DC readout parameters
+readout_len = 2 * u.us
+readout_amp = 0.0
+IV_scale_factor = 0.5e-9  # in A/V
+
+# Reflectometry
+resonator_IF = 151 * u.MHz
+reflectometry_readout_length = readout_len
+reflectometry_readout_amp = 30 * u.mV
+
+# Time of flight
+time_of_flight = 24
+
+######################
+#      DC GATES      #
+######################
+
+## Section defining the points from the charge stability map - can be done in the config
+# Relevant points in the charge stability map as ["P1", "P2"] in V
+level_init = [0.1, -0.1, 0.1, 0.2]
+level_manip = [0.2, -0.2]
+level_readout = [0.1, -0.1]
+
+# Duration of each step in ns
+duration_init = 30000
+duration_manip = 100000
+duration_readout = readout_len + 16
+duration_compensation_pulse = 4 * u.us
+
+# Step parameters
+step_length = 16
+P1_step_amp = 0.25
+P2_step_amp = 0.25
+charge_sensor_amp = 0.25
+
+# Time to ramp down to zero for sticky elements in ns
+hold_offset_duration = 4
+bias_tee_cut_off_frequency = 10 * u.kHz
+
+######################
+#    QUBIT PULSES    #
+######################
+# Durations in ns
+pi_length = 32
+pi_half_length = 16
+# Amplitudes in V
+pi_amps = [0.27, -0.27]
+pi_half_amps = [0.27, -0.27]
+
+
+#############################################
+#                  Config                   #
+#############################################
+config = {
+    "version": 1,
+    "controllers": {
+        "con1": {
+            "analog_outputs": {
+                1: {"offset": 0.0},  # P1
+                2: {"offset": 0.0},  # P2
+                3: {"offset": 0.0},  # Sensor gate
+                4: {"offset": 0.0},  # Sensor gate
+                9: {"offset": 0.0},  # RF reflectometry
+                10: {"offset": 0.0},  # DC readout
+            },
+            "digital_outputs": {
+                1: {},  # TTL for QDAC
+                2: {},  # TTL for QDAC
+            },
+            "analog_inputs": {
+                1: {"offset": 0.0, "gain_db": 0},  # RF reflectometry input
+                2: {"offset": 0.0, "gain_db": 0},  # DC readout input
+            },
+        },
+    },
+    "elements": {
+        "P1": {
+            "singleInput": {
+                "port": ("con1", 1),
+            },
+            "operations": {
+                "step": "P1_step_pulse",
+                "pi": "P1_pi_pulse",
+                "pi_half": "P1_pi_half_pulse",
+            },
+        },
+        "P1_sticky": {
+            "singleInput": {
+                "port": ("con1", 1),
+            },
+            "sticky": {"analog": True, "duration": hold_offset_duration},
+            "operations": {
+                "step": "P1_step_pulse",
+            },
+        },
+        "P3_sticky": {
+            "singleInput": {
+                "port": ("con1", 3),
+            },
+            "sticky": {"analog": True, "duration": hold_offset_duration},
+            "operations": {
+                "step": "P1_step_pulse",
+            },
+        },
+        "P4_sticky": {
+            "singleInput": {
+                "port": ("con1", 4),
+            },
+            "sticky": {"analog": True, "duration": hold_offset_duration},
+            "operations": {
+                "step": "P1_step_pulse",
+            },
+        },
+        "P2": {
+            "singleInput": {
+                "port": ("con1", 2),
+            },
+            "operations": {
+                "step": "P2_step_pulse",
+                "pi": "P2_pi_pulse",
+                "pi_half": "P2_pi_half_pulse",
+            },
+        },
+        "P2_sticky": {
+            "singleInput": {
+                "port": ("con1", 2),
+            },
+            "sticky": {"analog": True, "duration": hold_offset_duration},
+            "operations": {
+                "step": "P2_step_pulse",
+            },
+        },
+        "sensor_gate": {
+            "singleInput": {
+                "port": ("con1", 3),
+            },
+            "operations": {
+                "step": "bias_charge_pulse",
+            },
+        },
+        "sensor_gate_sticky": {
+            "singleInput": {
+                "port": ("con1", 3),
+            },
+            "sticky": {"analog": True, "duration": hold_offset_duration},
+            "operations": {
+                "step": "bias_charge_pulse",
+            },
+        },
+        "qdac_trigger1": {
+            "digitalInputs": {
+                "trigger": {
+                    "port": ("con1", 1),
+                    "delay": 0,
+                    "buffer": 0,
+                }
+            },
+            "operations": {
+                "trigger": "trigger_pulse",
+            },
+        },
+        "qdac_trigger2": {
+            "digitalInputs": {
+                "trigger": {
+                    "port": ("con1", 2),
+                    "delay": 0,
+                    "buffer": 0,
+                }
+            },
+            "operations": {
+                "trigger": "trigger_pulse",
+            },
+        },
+        "tank_circuit": {
+            "singleInput": {
+                "port": ("con1", 9),
+            },
+            "intermediate_frequency": resonator_IF,
+            "operations": {
+                "readout": "reflectometry_readout_pulse",
+            },
+            "outputs": {
+                "out1": ("con1", 1),
+                "out2": ("con1", 2),
+            },
+            "time_of_flight": time_of_flight,
+            "smearing": 0,
+        },
+        "TIA": {
+            "singleInput": {
+                "port": ("con1", 10),
+            },
+            "operations": {
+                "readout": "readout_pulse",
+            },
+            "outputs": {
+                "out1": ("con1", 1),
+                "out2": ("con1", 2),
+            },
+            "time_of_flight": time_of_flight,
+            "smearing": 0,
+        },
+    },
+    "pulses": {
+        "P1_pi_pulse": {
+            "operation": "control",
+            "length": pi_length,
+            "waveforms": {
+                "single": "P1_pi_wf",
+            },
+        },
+        "P1_pi_half_pulse": {
+            "operation": "control",
+            "length": pi_half_length,
+            "waveforms": {
+                "single": "P1_pi_half_wf",
+            },
+        },
+        "P2_pi_pulse": {
+            "operation": "control",
+            "length": pi_length,
+            "waveforms": {
+                "single": "P2_pi_wf",
+            },
+        },
+        "P2_pi_half_pulse": {
+            "operation": "control",
+            "length": pi_half_length,
+            "waveforms": {
+                "single": "P2_pi_half_wf",
+            },
+        },
+        "P1_step_pulse": {
+            "operation": "control",
+            "length": step_length,
+            "waveforms": {
+                "single": "P1_step_wf",
+            },
+        },
+        "P2_step_pulse": {
+            "operation": "control",
+            "length": step_length,
+            "waveforms": {
+                "single": "P2_step_wf",
+            },
+        },
+        "bias_charge_pulse": {
+            "operation": "control",
+            "length": step_length,
+            "waveforms": {
+                "single": "charge_sensor_step_wf",
+            },
+        },
+        "trigger_pulse": {
+            "operation": "control",
+            "length": 1000,
+            "digital_marker": "ON",
+        },
+        "reflectometry_readout_pulse": {
+            "operation": "measurement",
+            "length": reflectometry_readout_length,
+            "waveforms": {
+                "single": "reflect_wf",
+            },
+            "integration_weights": {
+                "cos": "cosine_weights",
+                "sin": "sine_weights",
+            },
+            "digital_marker": "ON",
+        },
+        "readout_pulse": {
+            "operation": "measurement",
+            "length": readout_len,
+            "waveforms": {
+                "single": "readout_pulse_wf",
+            },
+            "integration_weights": {
+                "constant": "constant_weights",
+            },
+            "digital_marker": "ON",
+        },
+    },
+    "waveforms": {
+        "P1_pi_wf": {"type": "constant", "sample": pi_amps[0] - level_manip[0]},
+        "P1_pi_half_wf": {"type": "constant", "sample": pi_half_amps[0] - level_manip[0]},
+        "P2_pi_wf": {"type": "constant", "sample": pi_amps[1] - level_manip[1]},
+        "P2_pi_half_wf": {"type": "constant", "sample": pi_half_amps[1] - level_manip[1]},
+        "P1_step_wf": {"type": "constant", "sample": P1_step_amp},
+        "P2_step_wf": {"type": "constant", "sample": P2_step_amp},
+        "charge_sensor_step_wf": {"type": "constant", "sample": charge_sensor_amp},
+        "readout_pulse_wf": {"type": "constant", "sample": readout_amp},
+        "reflect_wf": {"type": "constant", "sample": reflectometry_readout_amp},
+        "zero_wf": {"type": "constant", "sample": 0.0},
+    },
+    "digital_waveforms": {
+        "ON": {"samples": [(1, 0)]},
+    },
+    "integration_weights": {
+        "constant_weights": {
+            "cosine": [(1, readout_len)],
+            "sine": [(0.0, readout_len)],
+        },
+        "cosine_weights": {
+            "cosine": [(1.0, reflectometry_readout_length)],
+            "sine": [(0.0, reflectometry_readout_length)],
+        },
+        "sine_weights": {
+            "cosine": [(0.0, reflectometry_readout_length)],
+            "sine": [(1.0, reflectometry_readout_length)],
+        },
+    },
+}
---
+++ tests_against_server/voltage_gate_sequence/test_compensation.py
@@ -0,0 +1,166 @@
+from qm.qua import *
+from qm import QuantumMachinesManager
+from qm import SimulationConfig
+from configuration import *
+from qualang_tools.results import progress_counter, fetching_tool
+from qualang_tools.plot import interrupt_on_close
+from qualang_tools.loops import from_array
+from qualang_tools.addons.variables import assign_variables_to_element
+import matplotlib.pyplot as plt
+
+
+###################
+# The QUA program #
+###################
+
+n_avg = 1000
+# Pulse duration sweep in ns - must be larger than 4 clock cycles
+durations = np.arange(52, 200, 4)
+# Pulse amplitude sweep (as a pre-factor of the qubit pulse amplitude) - must be within [-2; 2)
+ramp_durations = np.arange(16, 200, 4)
+
+# Add the relevant voltage points describing the "slow" sequence (no qubit pulse)
+seq = VoltageGateSequence(config, ["P1_sticky", "P2_sticky", "P3_sticky", "P4_sticky"])
+seq.add_points("initialization", level_init, duration_init)
+# seq.add_points("idle", level_manip, duration_manip)
+# seq.add_points("readout", level_readout, duration_readout)
+
+with program() as prog:
+    n = declare(int)  # QUA integer used as an index for the averaging loop
+    t = declare(int)  # QUA variable for the qubit pulse duration
+    t_R = declare(int)  # QUA variable for the qubit drive amplitude pre-factor
+    a = declare(fixed, value=0.07)
+    b = declare(fixed, value=0.01)
+
+    # Navigate through the charge stability map
+    seq.add_step(voltage_point_name="initialization", level=[a, -a, b, -b])
+    # seq.add_step(voltage_point_name="initialization", level=[0.07, -0.07, 0.01, -0.01])
+    # seq.add_step(voltage_point_name="idle", ramp_duration=t_R, duration=t)
+    # seq.add_step(voltage_point_name="readout", ramp_duration=t_R, duration=t)
+    seq.add_compensation_pulse(duration=60000)
+    # Measure the dot right after the qubit manipulation
+    # wait(duration_init * u.ns + 2*(t_R>>2) + (t >> 2), "tank_circuit")
+    # measure("readout", "tank_circuit", None)
+    seq.ramp_to_zero()
+
+
+#####################################
+#  Open Communication with the QOP  #
+#####################################
+qmm = QuantumMachinesManager(host="172.16.33.101", cluster_name="CS_2")
+from qm.qua import *
+import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter
+from typing import Union
+from numpy.typing import NDArray
+import numpy as np
+
+
+def round_to_fixed(x, number_of_bits=12):
+    """
+    function which rounds 'x' to 'number_of_bits' of precision to help reduce the accumulation of fixed point arithmetic errors
+    """
+    return round((2**number_of_bits) * x) / (2**number_of_bits)
+
+
+def get_filtered_voltage(
+    voltage_list: Union[NDArray, list], step_duration: float, bias_tee_cut_off_frequency: float, plot: bool = False
+):
+    """Get the voltage after filtering through the bias-tee
+
+    :param voltage_list: List of voltages outputted by the OPX in V.
+    :param step_duration: Duration of each step in s.
+    :param bias_tee_cut_off_frequency: Cut-off frequency of the bias-tee in Hz.
+    :param plot: Flag to plot the voltage values if set to True.
+    :return: the filtered and unfiltered voltage lists with 1Gs/s sampling rate.
+    """
+
+    def high_pass(data, f_cutoff):
+        res = butter(1, f_cutoff, btype="high", analog=False)
+        return lfilter(res[0], res[1], data)
+
+    y = [val for val in voltage_list for _ in range(int(step_duration * 1e9))]
+    y_filtered = high_pass(y, bias_tee_cut_off_frequency * 1e-9)
+    if plot:
+        # plt.figure()
+        plt.plot(y, label="before bias-tee")
+        plt.plot(y_filtered, label="after bias-tee")
+        plt.xlabel("Time [ns]")
+        plt.ylabel("Voltage [V]")
+        plt.legend()
+    print(f"Error: {np.mean(np.abs((y-y_filtered)/(max(y)-min(y))))*100:.2f} %")
+    return y, y_filtered
+
+
+###########################
+# Run or Simulate Program #
+###########################
+
+simulate = True
+
+if simulate:
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=100000 // 4)  # In clock cycles = 4ns
+    # Simulate blocks python until the simulation is done
+    job = qmm.simulate(config, prog, simulation_config)
+    # Plot the simulated samples
+    plt.figure()
+    plt.subplot(211)
+    job.get_simulated_samples().con1.plot()
+    plt.axhline(level_init[0], color="k", linestyle="--")
+    plt.axhline(level_manip[0], color="k", linestyle="--")
+    plt.axhline(level_readout[0], color="k", linestyle="--")
+    plt.axhline(level_init[1], color="k", linestyle="--")
+    plt.axhline(level_manip[1], color="k", linestyle="--")
+    plt.axhline(level_readout[1], color="k", linestyle="--")
+    # plt.yticks(
+    #     [
+    #         level_readout[1],
+    #         level_manip[1],
+    #         level_init[1],
+    #         0.0,
+    #         level_init[0],
+    #         level_manip[0],
+    #         level_readout[0],
+    #     ],
+    #     ["readout", "manip", "init", "0", "init", "manip", "readout"],
+    # )
+    plt.legend("")
+    plt.subplot(212)
+    get_filtered_voltage(job.get_simulated_samples().con1.analog["1"], 1e-9, 1000, True)
+
+else:
+    # Open a quantum machine to execute the QUA program
+    qm = qmm.open_qm(config)
+    # Send the QUA program to the OPX, which compiles and executes it - Execute does not block python!
+    job = qm.execute(prog)
+    # Get results from QUA program and initialize live plotting
+    results = fetching_tool(job, data_list=["I", "Q", "dc_signal", "iteration"], mode="live")
+    # Live plotting
+    fig = plt.figure()
+    interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
+    while results.is_processing():
+        # Fetch the data from the last OPX run corresponding to the current slow axis iteration
+        I, Q, DC_signal, iteration = results.fetch_all()
+        # Convert results into Volts
+        S = u.demod2volts(I + 1j * Q, reflectometry_readout_length, single_demod=True)
+        R = np.abs(S)  # Amplitude
+        phase = np.angle(S)  # Phase
+        DC_signal = u.demod2volts(DC_signal, readout_len, single_demod=True)
+        # Progress bar
+        progress_counter(iteration, n_avg)
+        # Plot data
+        plt.subplot(121)
+        plt.cla()
+        plt.title(r"$R=\sqrt{I^2 + Q^2}$ [V]")
+        plt.pcolor(ramp_durations, durations, R)
+        plt.xlabel("Ramp duration [ns]")
+        plt.ylabel("Interaction pulse duration [ns]")
+        plt.subplot(122)
+        plt.cla()
+        plt.title("Phase [rad]")
+        plt.pcolor(ramp_durations, durations, phase)
+        plt.xlabel("Ramp duration [ns]")
+        plt.ylabel("Interaction duration [ns]")
+        plt.tight_layout()
+        plt.pause(0.1)
