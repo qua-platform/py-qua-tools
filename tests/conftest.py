106da8b8d60b6489e6ad4b8fc6f3ae49489cb045
TheoQM <theo@quantum-machines.co>
2025-08-01 12:12:59 +0200

    TEST: new implementation of capabilities

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,12 @@ from contextlib import contextmanager
 
 import pytest
 
-from qm.api.models.capabilities import ServerCapabilities
+from qm.api.models.capabilities import QopCaps, ServerCapabilities
 from qm.api.models.info import QuaMachineInfo, ImplementationInfo
 from qm.containers.capabilities_container import create_capabilities_container
+from qm import QuantumMachinesManager
 
-
+QuantumMachinesManager.set_capabilities_offline()
 @contextmanager
 def ignore_deprecation_warnings():
     warnings.simplefilter("ignore")
@@ -15,10 +16,8 @@ def ignore_deprecation_warnings():
     warnings.simplefilter("default")
 
 
-# @pytest.fixture(autouse=True)
-# def capability_container():
-#     container = create_capabilities_container(QuaMachineInfo([], ImplementationInfo("", "", "")))
-#     container.capabilities.override(
-#         ServerCapabilities(True, True, True, True, True, True, True, True, True, True, True, True, True, True, 1, True)
-#     )
-#     return container
+@pytest.fixture(autouse=True)
+def capability_container():
+    capabilities = QopCaps.get_all()
+    capabilities_qop_names = [cap.qop_name for cap in capabilities]
+    return create_capabilities_container(QuaMachineInfo(capabilities_qop_names, ImplementationInfo("", "", "")))
