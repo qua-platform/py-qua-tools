95f7f433e4fd3006a87700eba251fdae088f6995
Deepak Khurana <119570568+Deepakkhurrana@users.noreply.github.com>
2025-08-27 19:12:06 +0200

    add TWPA type

--- qualang_tools/wirer/connectivity/types.py
+++ qualang_tools/wirer/connectivity/types.py
@@ -2,6 +2,8 @@ from typing import Union, List, Tuple
 
 QubitType = int
 QubitPairType = Tuple[int, int]
+TwpaType = int
 
 QubitsType = Union[QubitType, List[QubitType]]
 QubitPairsType = Union[QubitPairType, List[QubitPairType]]
+TwpasType = Union[TwpaType, List[TwpaType]]
