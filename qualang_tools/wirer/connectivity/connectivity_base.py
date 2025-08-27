b817ebf8fd0417c9a411d253f55253780f800bc1
Deepak Khurana <119570568+Deepakkhurrana@users.noreply.github.com>
2025-08-27 19:10:38 +0200

    add TWPA componant

--- qualang_tools/wirer/connectivity/connectivity_base.py
+++ qualang_tools/wirer/connectivity/connectivity_base.py
@@ -1,8 +1,8 @@
 from typing import Dict, List, Union
 
 from .channel_spec import ChannelSpec
-from .element import Element, ElementId, QubitReference, QubitPairReference
-from .types import QubitsType, QubitPairsType
+from .element import Element, ElementId, QubitReference, QubitPairReference, TWPAReference
+from .types import QubitsType, QubitPairsType, TwpasType
 from .wiring_spec import WiringSpec, WiringFrequency, WiringIOType, WiringLineType
 
 
@@ -111,6 +111,32 @@ class ConnectivityBase:
         elements = []
         for qubit in qubits:
             id = QubitReference(qubit)
+            if id not in self.elements:
+                self.elements[id] = Element(id)
+            elements.append(self.elements[id])
+
+        return elements
+
+    def _make_twpa_elements(self, twpas: TwpasType):
+        """
+        Creates `Element` objects for a list of TWPAs.
+
+        This method constructs `Element` objects for each twpa reference in the provided `twpas` list
+        and adds them to the internal `self.elements` dictionary. If the element for a twpa already exists,
+        it will not be recreated.
+
+        Args:
+            twpas (TwpasType): A list or a single twpa reference to generate element objects for.
+
+        Returns:
+            List[Element]: A list of `Element` objects created or retrieved for the specified qubits.
+        """
+        if not isinstance(twpas, list):
+            twpas = [twpas]
+
+        elements = []
+        for twpa in twpas:
+            id = TWPAReference(twpa)
             if id not in self.elements:
                 self.elements[id] = Element(id)
             elements.append(self.elements[id])
