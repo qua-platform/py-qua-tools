8cddc550371dfb1dd496c066f21da7a8ba0f3a8e
ChloeBO-QM <Chloe.Bureau-Oxton@quantum-machines.co>
2025-09-02 16:59:31 +0200

    add condition for reset of _comp_offset

--- qualang_tools/voltage_gates/voltage_gate_sequence.py
+++ qualang_tools/voltage_gates/voltage_gate_sequence.py
@@ -514,7 +514,8 @@ class VoltageGateSequence:
                     )
 
             self.current_level[i] = amplitude
-            self._comp_offset[i] = 0
+            if self._compensation:
+                self._comp_offset[i] = 0
 
     def ramp_to_zero(self, duration: int = None):
         """Ramp all the gate voltages down to zero Volt and reset the averaged voltage derived for defining the compensation pulse.
