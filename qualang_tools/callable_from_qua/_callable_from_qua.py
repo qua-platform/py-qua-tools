a58f91e3502303599f664018ef9dea14d1b2e631
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-10-27 20:47:04 +0100

    formatting fix

--- qualang_tools/callable_from_qua/_callable_from_qua.py
+++ qualang_tools/callable_from_qua/_callable_from_qua.py
@@ -45,6 +45,7 @@ def _get_program_scope():
             raise RuntimeError("Cannot get program scope. Please run this function inside a QUA program.")
     else:
         from qm.qua._scope_management.scopes_manager import scopes_manager
+
         # In QUA >= 1.2.3 NoScopeFoundException lives in qm.exceptions
         from qm.exceptions import NoScopeFoundException
 
