04e228858a76961a454233edee6f3200d829c454
TheoQM <theo@quantum-machines.co>
2026-03-26 08:47:36 +0100

    remove TwpaType

--- qualang_tools/wirer/connectivity/types.py
+++ qualang_tools/wirer/connectivity/types.py
@@ -3,9 +3,7 @@ from typing import Union, List, Tuple
 ElementType = Union[int, str]
 QubitType = Union[int, str]
 QubitPairType = Union[Tuple[int, int], Tuple[str, str], Tuple[int, str]]
-TwpaType = Union[int, str]
 
 ElementsType = Union[ElementType, List[ElementType]]
-TwpasType = Union[TwpaType, List[TwpaType]]
 QubitsType = Union[QubitType, List[QubitType]]
 QubitPairsType = Union[QubitPairType, List[QubitPairType]]
