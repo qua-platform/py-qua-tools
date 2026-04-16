127731ebcbb2351f6b8602b24e5dfb4b787f9f63
natangqm <natan.gold@quantum-machines.co>
2026-04-16 11:28:33 +0300

    format

--- qualang_tools/results/__init__.py
+++ qualang_tools/results/__init__.py
@@ -5,4 +5,11 @@ from qualang_tools.results.results import wait_until_job_is_paused
 from qualang_tools.results.data_handler import DataHandler, data_processors
 from qualang_tools.results.sweep_program_processing.qua_iterable_postprocess import fetch_xarray_data
 
-__all__ = ["fetching_tool", "progress_counter", "wait_until_job_is_paused", "DataHandler", "data_processors", "fetch_xarray_data"]
+__all__ = [
+    "fetching_tool",
+    "progress_counter",
+    "wait_until_job_is_paused",
+    "DataHandler",
+    "data_processors",
+    "fetch_xarray_data",
+]
